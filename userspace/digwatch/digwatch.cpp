42de0507fa9d01dd93e3c37fbeee4706a886fff8
Henri DF <henridf@gmail.com>
2016-04-12 16:01:52 -0700

    search for yaml config file
    In order:
    1) cmdline opt
    2) in-tree path
    3) /etc/digwatch.yaml

--- userspace/digwatch/digwatch.cpp
+++ userspace/digwatch/digwatch.cpp
@@ -173,6 +173,7 @@ int digwatch_init(int argc, char **argv)
 	string lua_main_filename;
 	string output_name = "stdout";
 	string infile;
+	string conf_filename;
 	string lua_dir = DIGWATCH_LUA_DIR;
 	lua_State* ls = NULL;
 
@@ -192,7 +193,7 @@ int digwatch_init(int argc, char **argv)
 		// Parse the args
 		//
 		while((op = getopt_long(argc, argv,
-                                        "ho:r:",
+                                        "c:ho:R:",
                                         long_options, &long_index)) != -1)
 		{
 			switch(op)
@@ -200,6 +201,9 @@ int digwatch_init(int argc, char **argv)
 			case 'h':
 				usage();
 				goto exit;
+			case 'c':
+				conf_filename = optarg;
+				break;
 			case 'o':
 				valid = std::find(valid_output_names.begin(), valid_output_names.end(), optarg) != valid_output_names.end();
 				if (!valid)
@@ -250,8 +254,49 @@ int digwatch_init(int argc, char **argv)
 
 		}
 
+		ifstream* conf_stream;
+		if (conf_filename.size())
+		{
+			conf_stream = new ifstream(conf_filename);
+			if (!conf_stream->good())
+			{
+				fprintf(stderr, "Could not find configuration file at %s \n", conf_filename.c_str());
+				result = EXIT_FAILURE;
+				goto exit;
+			}
+		}
+		else
+		{
+			conf_stream = new ifstream(DIGWATCH_SOURCE_CONF_FILE);
+			if (conf_stream->good())
+			{
+				conf_filename = DIGWATCH_SOURCE_CONF_FILE;
+			}
+			else
+			{
+				conf_stream = new ifstream(DIGWATCH_INSTALL_CONF_FILE);
+				if (conf_stream->good())
+				{
+					conf_filename = DIGWATCH_INSTALL_CONF_FILE;
+				}
+				else
+				{
+					conf_filename = "";
+				}
+			}
+		}
+
 		digwatch_configuration config;
-		config.init();
+		if (conf_filename.size())
+		{
+			cout << "Using configuration file " + conf_filename + "\n";
+			config.init(conf_filename);
+		}
+		else
+		{
+			cout << "No configuration file found, proceeding with defaults\n";
+			config.init();
+		}
 
 		if(signal(SIGINT, signal_callback) == SIG_ERR)
 		{
