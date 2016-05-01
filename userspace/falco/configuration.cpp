bde9631cd4864d4a45d1c7fb1a7fb831bb98bf9c
Henri DF <henridf@gmail.com>
2016-05-01 23:13:28 +0000

    More falco->digwatch renaming

--- userspace/falco/configuration.cpp
+++ userspace/falco/configuration.cpp
@@ -1,5 +1,5 @@
 #include "configuration.h"
-#include "config_digwatch.h"
+#include "config_falco.h"
 #include "sinsp.h"
 #include "logger.h"
 
@@ -19,7 +19,7 @@ void digwatch_configuration::init(string conf_filename)
 	string m_config_file = conf_filename;
 	m_config = new yaml_configuration(m_config_file);
 
-	m_rules_filename = m_config->get_scalar<string>("rules_file", "/etc/digwatch_rules.conf");
+	m_rules_filename = m_config->get_scalar<string>("rules_file", "/etc/falco_rules.conf");
 	m_json_output = m_config->get_scalar<bool>("json_output", false);
 
 	output_config file_output;
