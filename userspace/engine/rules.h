644f017b2abaf74ccd84af2afead194bbfca1534
Mark Stemm <mark.stemm@sysdig.com>
2016-10-07 10:51:25 -0700

    Add license comments to all source code.
    Add comment blocks to all source code w/ our gpl copyright notice.

--- userspace/engine/rules.h
+++ userspace/engine/rules.h
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2016 Draios inc.
+
+This file is part of falco.
+
+falco is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+falco is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with falco.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include <list>
