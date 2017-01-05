5636f8e25a51edd5227bfa13d457d15e93a48ac3
Mark Stemm <mark.stemm@gmail.com>
2017-01-05 11:08:39 -0800

    Add a local dockerfile variant.
    Add a local dockerfile variant that allows creating an image from a
    local .deb package.

---
+++ docker/local/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -e
+
+# Set the SYSDIG_SKIP_LOAD variable to skip loading the sysdig kernel module
+
+if [[ -z "${SYSDIG_SKIP_LOAD}" ]]; then
+    echo "* Setting up /usr/src links from host"
+
+    for i in $(ls $SYSDIG_HOST_ROOT/usr/src)
+    do
+        ln -s $SYSDIG_HOST_ROOT/usr/src/$i /usr/src/$i
+    done
+
+    /usr/bin/sysdig-probe-loader
+fi
+
+exec "$@"
