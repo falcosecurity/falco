422b2abf167441c2c7849bc81fe31a5880c28206
Néstor Salceda <nestor.salceda@gmail.com>
2018-09-06 17:32:33 +0200

    Add event-name to job name for avoid collisions among captures

--- integrations/kubernetes-response-engine/playbooks/specs/playbooks/start_sysdig_capture_for_container_playbook_spec.py
+++ integrations/kubernetes-response-engine/playbooks/specs/playbooks/start_sysdig_capture_for_container_playbook_spec.py
@@ -16,12 +16,14 @@ with description(playbooks.StartSysdigCaptureForContainer) as self:
     with it('add starts capturing job in same node than Pod alerted'):
         pod_name = 'any pod name'
         container_id = 'any container id'
+        event_time = 'any event time'
         alert = {'output_fields': {
             'k8s.pod.name': pod_name,
-            'container.id': container_id
+            'container.id': container_id,
+            'evt.time': event_time,
         }}
 
         self.playbook.run(alert)
 
         expect(self.k8s_client.start_sysdig_capture_for)\
-            .to(have_been_called_with(pod_name, container_id))
+            .to(have_been_called_with(pod_name, container_id, event_time))
