2352b96d6b1b08fd9529cb991c40ada9cd696601
Mark Stemm <mark.stemm@sysdig.com>
2018-09-20 11:47:10 -0700

    Change license to Apache 2.0 (#419)
    Replace references to GNU Public License to Apache license in:
    
     - COPYING file
     - README
     - all source code below falco
     - rules files
     - rules and code below test directory
     - code below falco directory
     - entrypoint for docker containers (but not the Dockerfiles)
    
    I didn't generally add copyright notices to all the examples files, as
    they aren't core falco. If they did refer to the gpl I changed them to
    apache.

--- examples/mitm-sh-installer/web_root/install-software.sh
+++ examples/mitm-sh-installer/web_root/install-software.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #
-# Copyright (C) 2013-2014 My Company inc.
+# Copyright (C) 2018 Draios Inc.
 #
-# This file is part of my-software
+# This file is part of falco.
 #
-# my-software is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# my-software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU General Public License
-# along with my-software.  If not, see <http://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
+
 set -e
 
 function install_rpm {
--- scripts/ignored-calls.sh
+++ scripts/ignored-calls.sh
@@ -1,3 +1,21 @@
+#
+# Copyright (C) 2018 Draios Inc.
+#
+# This file is part of falco.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 #!/bin/bash
 
 cat ../sysdig/userspace/libscap/syscall_info_table.c | grep EF_DROP_FALCO | sed -e 's/.*\"\(.*\)\".*/\1/'  | sort > ignored_syscall_info_table.txt
