9c574732f20a8a5b423308ed213ec111e4c9197c
Jean-Philippe Lachance <jplachance@coveo.com>
2019-02-21 13:40:11 -0500

    Update the Puppet module (#537)
    * Update the Puppet module:
    * Apply puppet-lint recommendations
    * Update the README since the project moved from draios to falcosecurity in GitHub
    * Move parameters in their own file
    + Add the DEB repository automatically
    + Add the EPEL repository automatically
    + Add a logrotate configuration
    * Update the configuration file with all the latest updates
    
    falco-CLA-1.0-contributing-entity: Coveo Solutions Inc.
    falco-CLA-1.0-signed-off-by: Jean-Philippe Lachance <jplachance@coveo.com>
    
    * * Set required modules versions properly
    * Set dependencies between classes
    * Set the class order
    * Apply mstemm's code review
    
    * * Drop the Puppet 3 support
    * Use a working version of puppetlabs-apt
    * Use dependencies to be compatible with Puppet 4.7 and above

--- integrations/puppet-module/sysdig-falco/manifests/config.pp
+++ integrations/puppet-module/sysdig-falco/manifests/config.pp
@@ -1,13 +1,12 @@
 # == Class: falco::config
 class falco::config inherits falco {
-
   file { '/etc/falco/falco.yaml':
-    notify  => Service['falco'],
     ensure  => file,
+    require => Class['falco::install'],
+    notify  => Service['falco'],
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template('falco/falco.yaml.erb'),
   }
-
 }
--- integrations/puppet-module/sysdig-falco/manifests/install.pp
+++ integrations/puppet-module/sysdig-falco/manifests/install.pp
@@ -1,6 +1,61 @@
 # == Class: falco::install
 class falco::install inherits falco {
+  case $::osfamily {
+    'Debian': {
+      apt::source { 'sysdig':
+        location => 'http://download.draios.com/stable/deb',
+        release  => 'stable-$(ARCH)/',
+        repos    => '',
+        key      => {
+          source => 'https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public',
+          id     => 'D27A72F32D867DF9300A241574490FD6EC51E8C4'
+        },
+      }
+
+      ensure_packages(["linux-headers-${::kernelrelease}"])
+
+      $dependencies = [
+        Apt::Source['sysdig'],
+        Package["linux-headers-${::kernelrelease}"],
+      ]
+    }
+    'RedHat': {
+      include 'epel'
+
+      yumrepo { 'sysdig':
+        baseurl  => 'http://download.draios.com/stable/rpm/$basearch',
+        descr    => 'Sysdig repository by Draios',
+        enabled  => 1,
+        gpgcheck => 0,
+      }
+
+      ensure_packages(["kernel-devel-${::kernelrelease}"])
+
+      $dependencies = [
+        Yumrepo['sysdig'],
+        Class['epel']
+      ]
+    }
+    default: {
+      $dependencies = []
+    }
+  }
+
   package { 'falco':
-      ensure => installed,
+    ensure  => $::falco::package_ensure,
+    require => $dependencies,
+  }
+
+  if ($::falco::file_output != undef) {
+    logrotate::rule { 'falco_output':
+      path          => $::falco::file_output[filename],
+      rotate        => 5,
+      rotate_every  => 'day',
+      size          => '1M',
+      missingok     => true,
+      compress      => true,
+      sharedscripts => true,
+      postrotate    => '/usr/bin/killall -USR1 falco'
+    }
   }
 }
