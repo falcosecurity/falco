6a6342adc626b2d57d25f93dd719439af2d5a8b4
Mark Stemm <mark.stemm@gmail.com>
2019-05-15 11:56:19 -0700

    Add instructions for k8s audit support in >= 1.13
    In K8s 1.13, there's a new mechanism for k8s audit logs using Audit
    Sinks, which can be created and managed like other k8s objects.
    
    Add instructions for enabling k8s audit logging for 1.13. The patching
    script is still required, as dynamic audit is not a GA feature and needs
    to be enabled. Also, the audit sink config is a template and needs to be
    filled in with the cluster ip address, like the webhook config for 1.11.

--- examples/k8s_audit_config/enable-k8s-audit.sh
+++ examples/k8s_audit_config/enable-k8s-audit.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 VARIANT=${1:-minikube}
+AUDIT_TYPE=${2:-static}
 
 if [ $VARIANT == "minikube" ]; then
     APISERVER_HOST=$(minikube ip)
@@ -23,14 +24,18 @@ if [ $VARIANT == "kops" ]; then
     fi
 fi
 
-echo "***Copying audit policy/webhook files to apiserver..."
+echo "***Copying apiserver config patch script to apiserver..."
 ssh -i $SSH_KEY $SSH_USER@$APISERVER_HOST "sudo mkdir -p /var/lib/k8s_audit && sudo chown $SSH_USER /var/lib/k8s_audit"
-scp -i $SSH_KEY audit-policy.yaml $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
-scp -i $SSH_KEY webhook-config.yaml $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
 scp -i $SSH_KEY apiserver-config.patch.sh $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
+
+if [ $AUDIT_TYPE == "static" ]; then
+    echo "***Copying audit policy/webhook files to apiserver..."
+    scp -i $SSH_KEY audit-policy.yaml $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
+    scp -i $SSH_KEY webhook-config.yaml $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
+fi
 
 echo "***Modifying k8s apiserver config (will result in apiserver restarting)..."
 
-ssh -i $SSH_KEY $SSH_USER@$APISERVER_HOST "sudo bash /var/lib/k8s_audit/apiserver-config.patch.sh $MANIFEST $VARIANT"
+ssh -i $SSH_KEY $SSH_USER@$APISERVER_HOST "sudo bash /var/lib/k8s_audit/apiserver-config.patch.sh $MANIFEST $VARIANT $AUDIT_TYPE"
 
 echo "***Done!"
