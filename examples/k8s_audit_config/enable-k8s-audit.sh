537e4b7e8daf33adda599a048239988e46d07f1d
Leonardo Di Donato <leodidonato@gmail.com>
2019-05-24 12:46:32 +0200

    chore(examples/k8s_audit_config): shebang + prevent globbing and word splitting

--- examples/k8s_audit_config/enable-k8s-audit.sh
+++ examples/k8s_audit_config/enable-k8s-audit.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
 VARIANT=${1:-minikube}
 AUDIT_TYPE=${2:-static}
 
-if [ $VARIANT == "minikube" ]; then
+if [ "$VARIANT" == "minikube" ]; then
     APISERVER_HOST=$(minikube ip)
     SSH_KEY=$(minikube ssh-key)
-    SSH_USER=docker
+    SSH_USER="docker"
     MANIFEST="/etc/kubernetes/manifests/kube-apiserver.yaml"
 fi
 
-if [ $VARIANT == "kops" ]; then
-#    APISERVER_HOST=api.your-kops-cluster-name.com
+if [ "$VARIANT" == "kops" ]; then
+    # APISERVER_HOST=api.your-kops-cluster-name.com
     SSH_KEY=~/.ssh/id_rsa
-    SSH_USER=admin
+    SSH_USER="admin"
     MANIFEST=/etc/kubernetes/manifests/kube-apiserver.manifest
 
     if [ -z "${APISERVER_HOST+xxx}" ]; then
@@ -25,22 +25,22 @@ if [ $VARIANT == "kops" ]; then
 fi
 
 echo "***Copying apiserver config patch script to apiserver..."
-ssh -i $SSH_KEY $SSH_USER@$APISERVER_HOST "sudo mkdir -p /var/lib/k8s_audit && sudo chown $SSH_USER /var/lib/k8s_audit"
-scp -i $SSH_KEY apiserver-config.patch.sh $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
+ssh -i $SSH_KEY "$SSH_USER@$APISERVER_HOST" "sudo mkdir -p /var/lib/k8s_audit && sudo chown $SSH_USER /var/lib/k8s_audit"
+scp -i $SSH_KEY apiserver-config.patch.sh "$SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit"
 
-if [ $AUDIT_TYPE == "static" ]; then
+if [ "$AUDIT_TYPE" == "static" ]; then
     echo "***Copying audit policy/webhook files to apiserver..."
-    scp -i $SSH_KEY audit-policy.yaml $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
-    scp -i $SSH_KEY webhook-config.yaml $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
+    scp -i $SSH_KEY audit-policy.yaml "$SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit"
+    scp -i $SSH_KEY webhook-config.yaml "$SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit"
 fi
 
-if [ $AUDIT_TYPE == "dynamic+log" ]; then
+if [ "$AUDIT_TYPE" == "dynamic+log" ]; then
     echo "***Copying audit policy file to apiserver..."
-    scp -i $SSH_KEY audit-policy.yaml $SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit
+    scp -i $SSH_KEY audit-policy.yaml "$SSH_USER@$APISERVER_HOST:/var/lib/k8s_audit"
 fi
 
 echo "***Modifying k8s apiserver config (will result in apiserver restarting)..."
 
-ssh -i $SSH_KEY $SSH_USER@$APISERVER_HOST "sudo bash /var/lib/k8s_audit/apiserver-config.patch.sh $MANIFEST $VARIANT $AUDIT_TYPE"
+ssh -i $SSH_KEY "$SSH_USER@$APISERVER_HOST" "sudo bash /var/lib/k8s_audit/apiserver-config.patch.sh $MANIFEST $VARIANT $AUDIT_TYPE"
 
 echo "***Done!"
