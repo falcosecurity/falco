a09f71b457c0e2065ded72ed959e4c9f169634da
Leonardo Di Donato <leodidonato@gmail.com>
2019-06-27 10:34:47 +0000

    new: dependency inject the timer for token bucket
    Co-Authored-By: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- userspace/engine/token_bucket.h
+++ userspace/engine/token_bucket.h
@@ -20,59 +20,63 @@ limitations under the License.
 #pragma once
 
 #include <cstdint>
+#include <functional>
+
+using token_timer = std::function<uint64_t()>;
 
 // A simple token bucket that accumulates tokens at a fixed rate and allows
 // for limited bursting in the form of "banked" tokens.
 class token_bucket
 {
-public:
-	token_bucket();
-	virtual ~token_bucket();
+  public:
+    token_bucket();
+    token_bucket(std::function<uint64_t()> timer);
+    virtual ~token_bucket();
 
-	//
-	// Initialize the token bucket and start accumulating tokens
-	//
-	void init(double rate, double max_tokens, uint64_t now = 0);
+    //
+    // Initialize the token bucket and start accumulating tokens
+    //
+    void init(double rate, double max_tokens, uint64_t now = 0);
 
-	//
-	// Try to claim tokens tokens from the token bucket, using a
-	// timestamp of now. Returns true if the tokens could be
-	// claimed. Also updates internal metrics.
-	//
-	bool claim(double tokens, uint64_t now);
+    //
+    // Try to claim tokens tokens from the token bucket, using a
+    // timestamp of now. Returns true if the tokens could be
+    // claimed. Also updates internal metrics.
+    //
+    bool claim(double tokens, uint64_t now);
 
-	// Simpler version of claim that claims a single token and
-	// uses the current time for now
-	bool claim();
+    // Simpler version of claim that claims a single token and
+    // uses the current time for now
+    bool claim();
 
-	// Return the current number of tokens available
-	double get_tokens();
+    // Return the current number of tokens available
+    double get_tokens();
 
-	// Return the last time someone tried to claim a token.
-	uint64_t get_last_seen();
+    // Return the last time someone tried to claim a token.
+    uint64_t get_last_seen();
 
-private:
+  private:
+    std::function<uint64_t()> m_timer;
 
-	//
-	// The number of tokens generated per second.
-	//
-	double m_rate;
+    //
+    // The number of tokens generated per second.
+    //
+    double m_rate;
 
-	//
-	// The maximum number of tokens that can be banked for future
-	// claim()s.
-	//
-	double m_max_tokens;
+    //
+    // The maximum number of tokens that can be banked for future
+    // claim()s.
+    //
+    double m_max_tokens;
 
-	//
-	// The current number of tokens
-	//
-	double m_tokens;
+    //
+    // The current number of tokens
+    //
+    double m_tokens;
 
-	//
-	// The last time claim() was called (or the object was created).
-	// Nanoseconds since the epoch.
-	//
-	uint64_t m_last_seen;
+    //
+    // The last time claim() was called (or the object was created).
+    // Nanoseconds since the epoch.
+    //
+    uint64_t m_last_seen;
 };
-
