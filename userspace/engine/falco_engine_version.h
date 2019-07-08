affb1086a3652fe4a465a0082d41d47813cec96a
Lorenzo Fontana <lo@linux.com>
2019-07-08 13:05:52 +0000

    update: fields checksum while adding ka.useragent
    Signed-off-by: Lorenzo Fontana <lo@linux.com>
    
    Co-authored-by: Leonardo Di Donato <leodidonato@gmail.com>

--- userspace/engine/falco_engine_version.h
+++ userspace/engine/falco_engine_version.h
@@ -24,4 +24,4 @@ limitations under the License.
 // This is the result of running "falco --list -N | sha256sum" and
 // represents the fields supported by this version of falco. It's used
 // at build time to detect a changed set of fields.
-#define FALCO_FIELDS_CHECKSUM "9b5557ec8f16f5606a1544573b152d211d5212f653ee039146836a17266ff449"
+#define FALCO_FIELDS_CHECKSUM "ceb069d9f9b2d4ebcc5de39bddc53b7af2e6b8f072edc293668fd6ac4e532413"
