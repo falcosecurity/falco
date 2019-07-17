e9232246329e23236fc6b4aac46e0f7a433a95c4
Leonardo Di Donato <leodidonato@gmail.com>
2019-07-17 16:59:44 +0000

    update: detect current falco version during travis testing
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- hack/build.sh
+++ hack/build.sh
@@ -4,10 +4,25 @@ set -xeuo pipefail
 
 SOURCE_DIR=$1
 BUILD_DIR=$2
-FALCOBUILDER_IMAGE=$3
+FALCOBUILDER_IMAGE="falcosecurity/falco-builder:chore-travis"
+FALCOTESTER_IMAGE="falcosecurity/falco-tester:chore-travis"
 
 docker run --user "$(id -u)":"$(id -g)" -v /etc/passwd:/etc/passwd:ro -e BUILD_TYPE="$BUILD_TYPE" -v "$SOURCE_DIR":/source -v "$BUILD_DIR":/build "$FALCOBUILDER_IMAGE" cmake
 docker run --user "$(id -u)":"$(id -g)" -v /etc/passwd:/etc/passwd:ro -e BUILD_TYPE="$BUILD_TYPE" -v "$SOURCE_DIR":/source -v "$BUILD_DIR":/build "$FALCOBUILDER_IMAGE" package
 docker run --user "$(id -u)":"$(id -g)" -v /etc/passwd:/etc/passwd:ro -e BUILD_TYPE="$BUILD_TYPE" -v "$SOURCE_DIR":/source -v "$BUILD_DIR":/build "$FALCOBUILDER_IMAGE" tests
 
-docker run -v /boot:/boot:ro -v /var/run/docker.sock:/var/run/docker.sock -v /etc/passwd:/etc/passwd:ro -e BUILD_TYPE="$BUILD_TYPE" -v "$SOURCE_DIR":/source -v "$BUILD_DIR":/build falcosecurity/falco-tester
+# Deduct currently built version
+CURRENT_FALCO_VERSION=$(docker run -v "$BUILD_DIR":/build -ti "$FALCOBUILDER_IMAGE" bash -c "./build/$BUILD_TYPE/userspace/falco/falco --version" | cut -d' ' -f3)
+# CURRENT_FALCO_VERSION="${CURRENT_FALCO_VERSION#"${CURRENT_FALCO_VERSION%%[![:space:]]*}"}"
+# CURRENT_FALCO_VERSION="${CURRENT_FALCO_VERSION%"${CURRENT_FALCO_VERSION##*[![:space:]]}"}"
+
+# Execute regression tests
+docker run \
+    -v /boot:/boot:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v "$SOURCE_DIR":/source \
+    -v "$BUILD_DIR":/build \
+    -e BUILD_TYPE="$BUILD_TYPE" \
+    -e FALCO_VERSION="$CURRENT_FALCO_VERSION" \
+    "$FALCOTESTER_IMAGE"
