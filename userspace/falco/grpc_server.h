25f5fcacae97d17073fece8eb92f9a4988f2294a
Lorenzo Fontana <lo@linux.com>
2019-09-04 18:34:05 +0200

    new(userspace/falco): grpc server event bus queue
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- userspace/falco/grpc_server.h
+++ userspace/falco/grpc_server.h
@@ -21,16 +21,30 @@ limitations under the License.
 #include <thread>
 #include <string>
 #include <atomic>
+#include <queue>
+
+#include "tbb/concurrent_queue.h"
 
 #include "falco_output.grpc.pb.h"
 #include "falco_output.pb.h"
 #include "grpc_context.h"
+
+using namespace tbb;
+
+typedef concurrent_queue<falco_output_response> falco_output_response_cq;
 
 class falco_grpc_server_impl
 {
 public:
 	falco_grpc_server_impl() = default;
 	~falco_grpc_server_impl() = default;
+
+	falco_output_response_cq& m_event_queue;
+
+	falco_grpc_server_impl(falco_output_response_cq& event_queue):
+		m_event_queue(event_queue)
+	{
+	}
 
 protected:
 	bool is_running();
@@ -44,11 +58,13 @@ private:
 class falco_grpc_server : public falco_grpc_server_impl
 {
 public:
-	falco_grpc_server(std::string server_addr, int threadiness):
+	falco_grpc_server(std::string server_addr, int threadiness, falco_output_response_cq& m_event_queue):
+		falco_grpc_server_impl(m_event_queue),
 		m_server_addr(server_addr),
 		m_threadiness(threadiness)
 	{
 	}
+
 	virtual ~falco_grpc_server() = default;
 
 	void thread_process(int thread_index);
@@ -65,7 +81,7 @@ private:
 	std::vector<std::thread> m_threads;
 };
 
-void start_grpc_server(std::string server_address, int threadiness);
+bool start_grpc_server(std::string server_address, int threadiness, falco_output_response_cq& output_event_queue);
 
 class request_context_base
 {
