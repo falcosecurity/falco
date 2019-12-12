02c0b7513c1653868015ecc3077bd0e52dd0a2f1
Lorenzo Fontana <lo@linux.com>
2019-12-12 15:42:13 +0100

    build: include Coverage and add headers
    Signed-off-by: Lorenzo Fontana <lo@linux.com>
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ cmake/modules/Coverage.cmake
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2019 The Falco Authors.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
+# Tests coverage
+option(FALCO_COVERAGE "Build test suite with coverage information" OFF)
+if(FALCO_COVERAGE)
+    if (NOT (("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU") OR ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")))
+        message(FATAL_ERROR "FALCO_COVERAGE requires GCC or Clang.")
+    endif()
+
+    message(STATUS "Building with coverage information")
+    add_compile_options(-g --coverage)
+    set(CMAKE_SHARED_LINKER_FLAGS "--coverage ${CMAKE_SHARED_LINKER_FLAGS}")
+    set(CMAKE_EXE_LINKER_FLAGS "--coverage ${CMAKE_EXE_LINKER_FLAGS}")
+endif()
