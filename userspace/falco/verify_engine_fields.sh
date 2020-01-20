739d79a1eb4e934f084dd355de0ce8478f2f24b1
Leonardo Di Donato <leodidonato@gmail.com>
2020-01-20 20:33:18 +0000

    chore: double-quoting verify fields variables
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- userspace/falco/verify_engine_fields.sh
+++ userspace/falco/verify_engine_fields.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 SOURCE_DIR=$1
 OPENSSL=$2
 
-if ! command -v ${OPENSSL} version > /dev/null 2>&1; then
+if ! command -v "${OPENSSL}" version > /dev/null 2>&1; then
     echo "No openssl command at ${OPENSSL}"
     exit 1
 fi
 
 NEW_CHECKSUM=$(./falco --list -N | ${OPENSSL} dgst -sha256 | awk '{print $2}')
-CUR_CHECKSUM=$(grep FALCO_FIELDS_CHECKSUM ${SOURCE_DIR}/userspace/engine/falco_engine_version.h | awk '{print $3}' | sed -e 's/"//g')
+CUR_CHECKSUM=$(grep FALCO_FIELDS_CHECKSUM "${SOURCE_DIR}/userspace/engine/falco_engine_version.h" | awk '{print $3}' | sed -e 's/"//g')
 
 
-if [ $NEW_CHECKSUM != $CUR_CHECKSUM ]; then
+if [ "$NEW_CHECKSUM" != "$CUR_CHECKSUM" ]; then
     echo "Set of fields supported has changed (new checksum $NEW_CHECKSUM != old checksum $CUR_CHECKSUM)."
     echo "Update checksum and/or version in falco_engine_version.h."
     exit 1
