ff53387023195ce0fd262c578a38e9699aef5427
Kris Nova <kris@nivenly.com>
2020-04-30 21:53:14 +0000

    feat(arm support): Building Falco on ARM
    This is a hybrid branch based on 0.22.1 and with changes from master for libgpr
    
    This compiles Falco on ARM
    This compiles the kernel module on ARM
    
    Signed-off-by: Kris Nova <kris@nivenly.com>

---
+++ build/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (C) 2019 The Falco Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
+cmake ../ \
+      -DBUILD_BPF=OFF \
+      -DBUILD_WARNINGS_AS_ERRORS="OFF" \
+      -DCMAKE_BUILD_TYPE="debug" \
+      -DCMAKE_INSTALL_PREFIX="/usr" \
+      -DDRAIOS_DEBUG_FLAGS="-D_DEBUG" \
+      -DFALCO_ETC_DIR="/etc/falco" \
+      -DUSE_BUNDLED_DEPS=OFF
+
