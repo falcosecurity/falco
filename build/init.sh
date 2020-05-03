825a66e4d44d03a39457bc1c75316741738407de
Kris Nova <kris@nivenly.com>
2020-05-03 08:37:58 +0000

    feat(build): Using bundled deps in init.sh
    Signed-off-by: Kris Nova <kris@nivenly.com>

--- build/init.sh
+++ build/init.sh
@@ -15,9 +15,9 @@
 cmake ../ \
       -DBUILD_BPF=OFF \
       -DBUILD_WARNINGS_AS_ERRORS="OFF" \
-      -DCMAKE_BUILD_TYPE="debug" \
+      -DCMAKE_BUILD_TYPE="release" \
       -DCMAKE_INSTALL_PREFIX="/usr" \
       -DDRAIOS_DEBUG_FLAGS="-D_DEBUG" \
       -DFALCO_ETC_DIR="/etc/falco" \
-      -DUSE_BUNDLED_DEPS=OFF
+      -DUSE_BUNDLED_DEPS=ON
 
