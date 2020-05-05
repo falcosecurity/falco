418dc9e985ca0825a3efc06b61f29abac544b68b
Mark Stemm <mark.stemm@gmail.com>
2020-05-04 17:39:15 -0700

    Allow exact matches for rule names
    Currently, when calling enable_rule, the provided rule name pattern is a
    substring match, that is if the rules file has a rule "My fantastic
    rule", and you call engine->enable_rule("fantastic", true), the rule
    will be enabled.
    
    This can cause problems if one rule name is a complete subset of another
    rule name e.g. rules "My rule" and "My rule is great", and calling
    engine->enable_rule("My rule", true).
    
    To allow for this case, add an alternate method enable_rule_exact() in
    both default ruleset and ruleset variants. In this case, the rule name
    must be an exact match.
    
    In the underlying ruleset code, add a "match_exact" option to
    falco_ruleset::enable() that denotes whether the substring is an exact
    or substring match.
    
    This doesn't change the default behavior of falco in any way, as the
    existing calls still use enable_rule().
    
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

--- userspace/engine/falco_engine.cpp
+++ userspace/engine/falco_engine.cpp
@@ -211,14 +211,29 @@ void falco_engine::load_rules_file(const string &rules_filename, bool verbose, b
 void falco_engine::enable_rule(const string &substring, bool enabled, const string &ruleset)
 {
 	uint16_t ruleset_id = find_ruleset_id(ruleset);
+	bool match_exact = false;
 
-	m_sinsp_rules->enable(substring, enabled, ruleset_id);
-	m_k8s_audit_rules->enable(substring, enabled, ruleset_id);
+	m_sinsp_rules->enable(substring, match_exact, enabled, ruleset_id);
+	m_k8s_audit_rules->enable(substring, match_exact, enabled, ruleset_id);
 }
 
 void falco_engine::enable_rule(const string &substring, bool enabled)
 {
 	enable_rule(substring, enabled, m_default_ruleset);
+}
+
+void falco_engine::enable_rule_exact(const string &rule_name, bool enabled, const string &ruleset)
+{
+	uint16_t ruleset_id = find_ruleset_id(ruleset);
+	bool match_exact = true;
+
+	m_sinsp_rules->enable(rule_name, match_exact, enabled, ruleset_id);
+	m_k8s_audit_rules->enable(rule_name, match_exact, enabled, ruleset_id);
+}
+
+void falco_engine::enable_rule_exact(const string &rule_name, bool enabled)
+{
+	enable_rule_exact(rule_name, enabled, m_default_ruleset);
 }
 
 void falco_engine::enable_rule_by_tag(const set<string> &tags, bool enabled, const string &ruleset)
