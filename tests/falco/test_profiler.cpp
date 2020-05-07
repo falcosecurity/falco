e8afe720638116c168330ae4826398a0b4169d7a
Lorenzo Fontana <lo@linux.com>
2020-05-07 13:08:07 +0200

    update(tests/falco): more complete profiler tests
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- tests/falco/test_profiler.cpp
+++ tests/falco/test_profiler.cpp
@@ -18,22 +18,71 @@ limitations under the License.
 #include <thread>
 #include <chrono>
 
+void profiler_test_do_experiment()
+{
+	PROFILEME();
+}
+
 void profiler_test_do_work()
 {
 	PROFILEME();
-	std::this_thread::sleep_for (std::chrono::seconds(1));
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void profiler_test_do_work2()
 {
 	PROFILEME();
-	std::this_thread::sleep_for (std::chrono::seconds(1));
+	profiler_test_do_work();
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
-TEST_CASE("profiler works", "[profiler]")
+TEST_CASE("profiler tests", "[profiler]")
 {
+	SECTION("profiler needs to be allocated before usage")
+	{
+		alloc_chunk();
+		REQUIRE(chunks.size() == 1);
+		REQUIRE(labels.empty());
+	}
+
+	SECTION("calling a profiled function results in labels and chunks increase")
+	{
+		profiler_test_do_work();
+		REQUIRE(labels.size() == 1);
+		REQUIRE(chunks.size() == 1);
+	}
+
+	SECTION("calling just another profiled function that calls the one previously called should only increase labels by one")
+	{
+		profiler_test_do_work2();
+		REQUIRE(labels.size() == 2);
+		REQUIRE(chunks.size() == 1);
+	}
+
+	SECTION("clean")
+	{
+		labels.clear();
+		chunks.clear();
+		REQUIRE(labels.empty());
+		REQUIRE(chunks.empty());
+	}
+}
+
+TEST_CASE("consecutive chunks allocation", "[profiler]")
+{
+	int expected_chunks = 10;
 	alloc_chunk();
-	profiler_test_do_work();
-	profiler_test_do_work2();
-	REQUIRE(labels.size() == 2);
+	for(int i = chunks.size(); i < (CHUNK_SIZE / 5) * expected_chunks; i++)
+	{
+		profiler_test_do_experiment();
+	}
+
+	REQUIRE(labels.size() == 1);
+	REQUIRE(chunks.size() == expected_chunks);
+
+	labels.clear();
+	chunks.clear();
+
+	REQUIRE(labels.empty());
+	REQUIRE(chunks.empty());
 }
