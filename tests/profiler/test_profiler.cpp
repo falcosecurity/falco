54050b2d4a4e4d47876125ef08c48ecea0b41d8d
Leonardo Di Donato <leodidonato@gmail.com>
2020-05-08 10:21:16 +0000

    update(tests/profiler): setup test for parents
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- tests/profiler/test_profiler.cpp
+++ tests/profiler/test_profiler.cpp
@@ -19,71 +19,157 @@ limitations under the License.
 #include <chrono>
 #include <hedley.h>
 
-void profiler_test_do_experiment()
+#include <cstdlib>
+#include <iostream>
+
+void profile_empty_func()
 {
 	PROFILEME();
 }
 
-void profiler_test_do_work()
+// void profiler_test_do_work()
+// {
+// 	PROFILEME();
+// 	std::this_thread::sleep_for(std::chrono::seconds(1));
+// }
+
+// void profiler_test_do_work2()
+// {
+// 	PROFILEME();
+// 	profiler_test_do_work();
+// 	std::this_thread::sleep_for(std::chrono::seconds(1));
+// }
+
+// TEST_CASE("profiler tests", "[profiler]")
+// {
+// 	SECTION("profiler needs to be allocated before usage")
+// 	{
+// 		alloc_chunk();
+// 		REQUIRE(chunks.size() == 1);
+// 		REQUIRE(labels.empty());
+// 	}
+
+// 	SECTION("calling a profiled function results in labels and chunks increase")
+// 	{
+// 		profiler_test_do_work();
+// 		REQUIRE(labels.size() == 1);
+// 		REQUIRE(chunks.size() == 1);
+// 	}
+
+// 	SECTION("calling just another profiled function that calls the one previously called should only increase labels by one")
+// 	{
+// 		profiler_test_do_work2();
+// 		REQUIRE(labels.size() == 2);
+// 		REQUIRE(chunks.size() == 1);
+// 	}
+
+// 	SECTION("clean")
+// 	{
+// 		labels.clear();
+// 		chunks.clear();
+// 		REQUIRE(labels.empty());
+// 		REQUIRE(chunks.empty());
+// 	}
+// }
+
+void BB()
 {
 	PROFILEME();
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::this_thread::sleep_for(std::chrono::microseconds(1));
 }
 
-void profiler_test_do_work2()
+void AA()
 {
 	PROFILEME();
-	profiler_test_do_work();
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	BB();
+	std::this_thread::sleep_for(std::chrono::microseconds(1));
 }
 
-TEST_CASE("profiler tests", "[profiler]")
+void CC()
 {
-	SECTION("profiler needs to be allocated before usage")
-	{
-		alloc_chunk();
-		REQUIRE(chunks.size() == 1);
-		REQUIRE(labels.empty());
-	}
-
-	SECTION("calling a profiled function results in labels and chunks increase")
-	{
-		profiler_test_do_work();
-		REQUIRE(labels.size() == 1);
-		REQUIRE(chunks.size() == 1);
-	}
-
-	SECTION("calling just another profiled function that calls the one previously called should only increase labels by one")
-	{
-		profiler_test_do_work2();
-		REQUIRE(labels.size() == 2);
-		REQUIRE(chunks.size() == 1);
-	}
-
-	SECTION("clean")
-	{
-		labels.clear();
-		chunks.clear();
-		REQUIRE(labels.empty());
-		REQUIRE(chunks.empty());
-	}
+	PROFILEME();
+	std::this_thread::sleep_for(std::chrono::microseconds(1));
 }
 
-TEST_CASE("consecutive chunks allocation", "[profiler]")
+void A()
 {
-	size_t expected_chunks_size = 10;
+	PROFILEME();
+	AA();
+	std::this_thread::sleep_for(std::chrono::microseconds(1));
+	BB();
+	std::this_thread::sleep_for(std::chrono::microseconds(1));
+	CC();
+	std::this_thread::sleep_for(std::chrono::microseconds(1));
+	AA();
+}
+
+TEST_CASE("aaa", "[profiler]")
+{
 	alloc_chunk();
-	for(int i = chunks.size(); i < (CHUNK_SIZE / CHUNK_ELEMENTS) * HEDLEY_STATIC_CAST(int, expected_chunks_size); i++)
+	A();
+
+	for(std::vector<int>::size_type j = 0; j != chunks.size(); j++)
 	{
-		profiler_test_do_experiment();
+		auto *c = chunks[j].begin;
+		for(int i = 0; i < CHUNK_SIZE; i++) // i < 70
+		{
+			char b[1024];
+			sprintf(b, "%ld - %03d: %u", j, i, c[i]);
+			std::cout << std::string(b) << std::endl;
+		}
 	}
 
-	REQUIRE(labels.size() == 1);
-	REQUIRE(chunks.size() == expected_chunks_size);
+	// Ordine costruttori:
+	// A
+	//     AA1
+	//         BB1
+	//     BB2
+	//     CC1
+	//     AA2
+	//         BB3
+
+	// 2147483648 -> 0
+	//      2147483649 -> 2147483648
+	//           2147483650 -> 2147483649
+	//      2147483650 -> 2147483648
+	//      2147483651 -> 2147483648
+	//      2147483649 -> 2147483648
+	//           2147483650 -> 2147483649
+
+	// Ordine distruttori:
+	// BB1
+	// AA1
+	// BB2
+	// CC1
+	// BB3
+	// AA2
+	// A
 
 	labels.clear();
 	chunks.clear();
+	REQUIRE(labels.empty());
+	REQUIRE(chunks.empty());
+}
 
+TEST_CASE("ccc", "[profiler]")
+{
+	alloc_chunk();
+	REQUIRE(chunks.size() == 1);
+
+	size_t expected_chunks = 2;
+	int how_many_times = (CHUNK_SIZE / CHUNK_ELEMENTS) * HEDLEY_STATIC_CAST(int, expected_chunks);
+
+	// Use lt (<) to avoid pre-allocation of a new chunk at last element of previous chunk
+	for(int i = 1; i < how_many_times; i++)
+	{
+		profile_empty_func();
+	}
+
+	REQUIRE(labels.size() == 1);
+	REQUIRE(chunks.size() == expected_chunks);
+
+	labels.clear();
+	chunks.clear();
 	REQUIRE(labels.empty());
 	REQUIRE(chunks.empty());
 }
