8dfc5da425552ff884eb5cfe003bbf79740c5985
Leonardo Di Donato <leodidonato@gmail.com>
2020-05-12 17:18:15 +0000

    update(tests/profiler): serialization/deserialization of profiler nodes
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- tests/profiler/test_profiler.cpp
+++ tests/profiler/test_profiler.cpp
@@ -24,7 +24,6 @@ limitations under the License.
 #include <iostream>
 
 #include <flatbuffers/flatbuffers.h>
-#include <flatbuffers/util.h>
 #include "profile_generated.h"
 
 // void profile_recurse(int times)
@@ -53,31 +52,31 @@ TEST_CASE("profiler stores labels and chunks", "[profiler]")
 {
 	SECTION("profiler needs to be allocated before usage")
 	{
-		alloc_chunk();
-		REQUIRE(chunks.size() == 1);
-		REQUIRE(labels.empty());
+		profiler::alloc_chunk();
+		REQUIRE(profiler::chunks.size() == 1);
+		REQUIRE(profiler::labels.empty());
 	}
 
 	SECTION("calling a profiled function results in labels and chunks increase")
 	{
 		profiler_sleep_1sec();
-		REQUIRE(labels.size() == 1);
-		REQUIRE(chunks.size() == 1);
+		REQUIRE(profiler::labels.size() == 1);
+		REQUIRE(profiler::chunks.size() == 1);
 	}
 
 	SECTION("calling just another profiled function that calls the one previously called should only increase labels by one")
 	{
 		profiler_sleep_2sec();
-		REQUIRE(labels.size() == 2);
-		REQUIRE(chunks.size() == 1);
+		REQUIRE(profiler::labels.size() == 2);
+		REQUIRE(profiler::chunks.size() == 1);
 	}
 
 	SECTION("clean")
 	{
-		labels.clear();
-		chunks.clear();
-		REQUIRE(labels.empty());
-		REQUIRE(chunks.empty());
+		profiler::labels.clear();
+		profiler::chunks.clear();
+		REQUIRE(profiler::labels.empty());
+		REQUIRE(profiler::chunks.empty());
 	}
 }
 
@@ -114,16 +113,16 @@ void profiler_fake_root_func()
 
 TEST_CASE("profiler computes parents (single chunk)", "[profiler]")
 {
-	alloc_chunk();
+	profiler::alloc_chunk();
 	profiler_fake_root_func();
 
-	REQUIRE(labels.size() == 4);
-	REQUIRE(chunks.size() == 1);
-	REQUIRE(chunks[0].begin[5] == 0); // profiler_fake_root_func is root (parent = 0)
+	REQUIRE(profiler::labels.size() == 4);
+	REQUIRE(profiler::chunks.size() == 1);
+	REQUIRE(profiler::chunks[0].begin[5] == 0); // profiler_fake_root_func is root (parent = 0)
 
 	auto predicate = [](int x) {
-		return [=](const label &l) {
-			return l.label == chunks[0].begin[x];
+		return [=](const profiler::label &l) {
+			return l.label == profiler::chunks[0].begin[x];
 		};
 	};
 
@@ -145,58 +144,60 @@ TEST_CASE("profiler computes parents (single chunk)", "[profiler]")
 	// AA2
 	// profile_fake_root
 
-	auto root = std::find_if(std::begin(labels), std::end(labels), predicate(0));
-	REQUIRE(root->func == "profiler_fake_root_func"); // function name is "profiler_fake_root_func"
-	REQUIRE(chunks[0].begin[5 + CHUNK_ELEMENTS] == root->label); // aa (call aa1) has parent "profiler_fake_root_func"
+	auto root = std::find_if(std::begin(profiler::labels), std::end(profiler::labels), predicate(0));
+	REQUIRE(root->func == "profiler_fake_root_func");		       // function name is "profiler_fake_root_func"
+	REQUIRE(profiler::chunks[0].begin[5 + CHUNK_ELEMENTS] == root->label); // aa (call aa1) has parent "profiler_fake_root_func"
 
 	{
-		auto it = std::find_if(std::begin(labels), std::end(labels), predicate(CHUNK_ELEMENTS));
-		REQUIRE(it->func == "aa"); // function name is "aa" (call aa1)
-		REQUIRE(chunks[0].begin[5 + (2 * CHUNK_ELEMENTS)] == it->label); // bb (call bb1) has parent "aa" (call aa1)
+		auto it = std::find_if(std::begin(profiler::labels), std::end(profiler::labels), predicate(CHUNK_ELEMENTS));
+		REQUIRE(it->func == "aa");						   // function name is "aa" (call aa1)
+		REQUIRE(profiler::chunks[0].begin[5 + (2 * CHUNK_ELEMENTS)] == it->label); // bb (call bb1) has parent "aa" (call aa1)
 	}
 
 	{
-		auto it = std::find_if(std::begin(labels), std::end(labels), predicate(2 * CHUNK_ELEMENTS));
-		REQUIRE(it->func == "bb"); // function name is "bb" (call bb1)
-		REQUIRE(chunks[0].begin[5 + (3 * CHUNK_ELEMENTS)] == root->label); // bb (call bb2) has parent "profiler_fake_root_func"
+		auto it = std::find_if(std::begin(profiler::labels), std::end(profiler::labels), predicate(2 * CHUNK_ELEMENTS));
+		REQUIRE(it->func == "bb");						     // function name is "bb" (call bb1)
+		REQUIRE(profiler::chunks[0].begin[5 + (3 * CHUNK_ELEMENTS)] == root->label); // bb (call bb2) has parent "profiler_fake_root_func"
 	}
 
 	{
-		auto it = std::find_if(std::begin(labels), std::end(labels), predicate(3 * CHUNK_ELEMENTS));
-		REQUIRE(it->func == "bb"); // function name is "bb" (call bb2)
-		REQUIRE(chunks[0].begin[5 + (4 * CHUNK_ELEMENTS)] == root->label); // cc (call cc1) has parent "profiler_fake_root_func"
+		auto it = std::find_if(std::begin(profiler::labels), std::end(profiler::labels), predicate(3 * CHUNK_ELEMENTS));
+		REQUIRE(it->func == "bb");						     // function name is "bb" (call bb2)
+		REQUIRE(profiler::chunks[0].begin[5 + (4 * CHUNK_ELEMENTS)] == root->label); // cc (call cc1) has parent "profiler_fake_root_func"
 	}
 
 	{
-		auto it = std::find_if(std::begin(labels), std::end(labels), predicate(4 * CHUNK_ELEMENTS));
-		REQUIRE(it->func == "cc"); // function name is "cc" (call cc1)
-		REQUIRE(chunks[0].begin[5 + (5 * CHUNK_ELEMENTS)] == root->label); // aa (call aa2) has parent "profiler_fake_root_func"
+		auto it = std::find_if(std::begin(profiler::labels), std::end(profiler::labels), predicate(4 * CHUNK_ELEMENTS));
+		REQUIRE(it->func == "cc");						     // function name is "cc" (call cc1)
+		REQUIRE(profiler::chunks[0].begin[5 + (5 * CHUNK_ELEMENTS)] == root->label); // aa (call aa2) has parent "profiler_fake_root_func"
 	}
 
 	{
-		auto it = std::find_if(std::begin(labels), std::end(labels), predicate(5 * CHUNK_ELEMENTS));
-		REQUIRE(it->func == "aa"); // function name is "aa" (call aa2)
-		REQUIRE(chunks[0].begin[5 + (6 * CHUNK_ELEMENTS)] == it->label); // bb (call bb3) has parent "aa" (call aa2)
+		auto it = std::find_if(std::begin(profiler::labels), std::end(profiler::labels), predicate(5 * CHUNK_ELEMENTS));
+		REQUIRE(it->func == "aa");						   // function name is "aa" (call aa2)
+		REQUIRE(profiler::chunks[0].begin[5 + (6 * CHUNK_ELEMENTS)] == it->label); // bb (call bb3) has parent "aa" (call aa2)
 	}
 
 	{
-		auto it = std::find_if(std::begin(labels), std::end(labels), predicate(6 * CHUNK_ELEMENTS));
+		auto it = std::find_if(std::begin(profiler::labels), std::end(profiler::labels), predicate(6 * CHUNK_ELEMENTS));
 		REQUIRE(it->func == "bb"); // function name is "bb" (call bb3)
 	}
 
-	for(std::vector<int>::size_type j = 0; j != chunks.size(); j++)
+	for(std::vector<int>::size_type j = 0; j != profiler::chunks.size(); j++)
 	{
-		auto *c = chunks[j].begin;
-		for(int i = 0; i < CHUNK_SIZE; i+=CHUNK_ELEMENTS)
+		auto *c = profiler::chunks[j].begin;
+		for(int i = 0; i < CHUNK_SIZE; i += CHUNK_ELEMENTS)
 		{
-			if (c[i] == 0) {
+			if(c[i] == 0)
+			{
 				break;
 			}
-			auto parent = c[i+5];
-			auto e = ((unsigned long long)c[i+4])|(((unsigned long long)c[i+3])<<32);
-			auto s = ((unsigned long long)c[i+2])|(((unsigned long long)c[i+1])<<32);
+			auto parent = c[i + 5];
+			auto e = ((unsigned long long)c[i + 4]) | (((unsigned long long)c[i + 3]) << 32);
+			auto s = ((unsigned long long)c[i + 2]) | (((unsigned long long)c[i + 1]) << 32);
 			char b[1024];
-			if (i == 0) {
+			if(i == 0)
+			{
 				sprintf(b, "#;idx;parent    ;function  ;clocks");
 				std::cout << std::string(b) << std::endl;
 			}
@@ -205,10 +206,10 @@ TEST_CASE("profiler computes parents (single chunk)", "[profiler]")
 		}
 	}
 
-	labels.clear();
-	chunks.clear();
-	REQUIRE(labels.empty());
-	REQUIRE(chunks.empty());
+	profiler::labels.clear();
+	profiler::chunks.clear();
+	REQUIRE(profiler::labels.empty());
+	REQUIRE(profiler::chunks.empty());
 }
 // TEST_CASE("profile computes recursive parents (more chunks)", "[profiler]")
 // {
@@ -244,46 +245,84 @@ TEST_CASE("profiler flatbuffer serialization deserialization", "[profiler]")
 {
 	flatbuffers::FlatBufferBuilder builder;
 
-	auto root_node = profiler_structures::NodeBuilder(builder);
-	root_node.add_func(builder.CreateString("main"));
-	root_node.add_file(builder.CreateString("falco.cpp"));
-	root_node.add_line(123);
-	root_node.add_cycles(750);
+	auto node_one = profiler::CreateNodeDirect(
+		builder,
+		"do_init",
+		"falco_engine.cpp",
+		345,
+		500);
 
-	auto node_one = profiler_structures::NodeBuilder(builder);
-	node_one.add_func(builder.CreateString("do_init"));
-	node_one.add_file(builder.CreateString("falco_engine.cpp"));
-	node_one.add_line(345);
-	node_one.add_cycles(500);
+	auto node_two_one = profiler::CreateNodeDirect(
+		builder,
+		"do_match2",
+		"ruleset.cpp",
+		690,
+		100);
 
-	auto node_two = profiler_structures::NodeBuilder(builder);
-	node_two.add_func(builder.CreateString("do_match"));
-	node_two.add_file(builder.CreateString("ruleset.cpp"));
-	node_two.add_line(678);
-	node_two.add_cycles(250);
+	std::vector<flatbuffers::Offset<profiler::Node>> third_level;
+	third_level.push_back(node_two_one);
 
-	auto node_two_one = profiler_structures::NodeBuilder(builder);
-	node_two_one.add_func(builder.CreateString("do_match"));
-	node_two_one.add_file(builder.CreateString("ruleset.cpp"));
-	node_two_one.add_line(690);
-	node_two_one.add_cycles(100);
+	auto node_two = profiler::CreateNodeDirect(
+		builder,
+		"do_match",
+		"ruleset.cpp",
+		678,
+		250,
+		&third_level);
 
-	std::vector<flatbuffers::Offset<profiler_structures::Node>> child_vector_node_two;
-	child_vector_node_two.push_back(node_two_one.Finish());
-	auto childs_node_two = builder.CreateVector(child_vector_node_two);
-	node_two_one.add_childs(childs_node_two);
+	std::vector<flatbuffers::Offset<profiler::Node>> second_level;
+	second_level.push_back(node_one);
+	second_level.push_back(node_two);
 
-	std::vector<flatbuffers::Offset<profiler_structures::Node>> child_vector;
-	child_vector.push_back(node_one.Finish());
-	child_vector.push_back(node_two.Finish());
+	auto root = profiler::CreateNodeDirect(
+		builder,
+		"main",
+		"falco.cpp",
+		123,
+		750,
+		&second_level);
 
-	auto childs = builder.CreateVector(child_vector);
+	profiler::FinishNodeBuffer(builder, root);
 
-	root_node.add_childs(childs);
+	SECTION("binary output has identifier")
+	{
+		REQUIRE(profiler::NodeBufferHasIdentifier(builder.GetBufferPointer()) == true);
+	}
 
-	profiler_structures::FinishNodeBuffer(builder, root_node.Finish());
+	SECTION("deserialization")
+	{
+		auto node = profiler::GetNode(builder.GetBufferPointer());
 
-	auto node = profiler_structures::GetNode(builder.GetBufferPointer());
+		REQUIRE(node->cycles() == 750);
+		REQUIRE(node->line() == 123);
+		REQUIRE(node->func()->str() == "main");
+		REQUIRE(node->file()->str() == "falco.cpp");
+		REQUIRE(node->childs() != nullptr);
+		REQUIRE(node->childs()->size() == 2);
+		REQUIRE(node->childs()->Get(0) == flatbuffers::GetTemporaryPointer(builder, node_one));
+		REQUIRE(node->childs()->Get(1) == flatbuffers::GetTemporaryPointer(builder, node_two));
 
-	REQUIRE(node->func()->str() == "main");
+		auto expect_one = node->childs()->Get(0);
+		REQUIRE(expect_one->cycles() == 500);
+		REQUIRE(expect_one->line() == 345);
+		REQUIRE(expect_one->func()->str() == "do_init");
+		REQUIRE(expect_one->file()->str() == "falco_engine.cpp");
+		REQUIRE(expect_one->childs() == nullptr);
+
+		auto expect_two = node->childs()->Get(1);
+		REQUIRE(expect_two->cycles() == 250);
+		REQUIRE(expect_two->line() == 678);
+		REQUIRE(expect_two->func()->str() == "do_match");
+		REQUIRE(expect_two->file()->str() == "ruleset.cpp");
+		REQUIRE(expect_two->childs() != nullptr);
+		REQUIRE(expect_two->childs()->size() == 1);
+		REQUIRE(expect_two->childs()->Get(0) == flatbuffers::GetTemporaryPointer(builder, node_two_one));
+
+		auto expect_two_one = expect_two->childs()->Get(0);
+		REQUIRE(expect_two_one->cycles() == 100);
+		REQUIRE(expect_two_one->line() == 690);
+		REQUIRE(expect_two_one->func()->str() == "do_match2");
+		REQUIRE(expect_two_one->file()->str() == "ruleset.cpp");
+		REQUIRE(expect_two_one->childs() == nullptr);
+	}
 }
