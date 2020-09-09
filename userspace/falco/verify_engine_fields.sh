aa6be4cb45c82176e23e9fd67ad2dc71bec7dada
Leonardo Di Donato <leodidonato@gmail.com>
2020-09-09 11:09:55 +0000

    build: engine fields checksum only when not building the minimal Falco
    Co-authored-by: Leonardo Grasso <me@leonardograsso.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- userspace/falco/verify_engine_fields.sh
+++ userspace/falco/verify_engine_fields.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/env/bash
 
 set -euo pipefail
 
@@ -6,7 +6,6 @@ SOURCE_DIR=$1
 
 NEW_CHECKSUM=$(./falco --list -N | sha256sum | awk '{print $1}')
 CUR_CHECKSUM=$(grep FALCO_FIELDS_CHECKSUM "${SOURCE_DIR}/userspace/engine/falco_engine_version.h" | awk '{print $3}' | sed -e 's/"//g')
-
 
 if [ "$NEW_CHECKSUM" != "$CUR_CHECKSUM" ]; then
     echo "Set of fields supported has changed (new checksum $NEW_CHECKSUM != old checksum $CUR_CHECKSUM)."
