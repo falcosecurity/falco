7b70f3c2ef7d4ba0b1a787d5f50db6c44c84bcaa
Leonardo Grasso <me@leonardograsso.com>
2020-09-22 17:44:03 +0200

    new(userspace/falco): stdout output C++ impl
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

---
+++ userspace/falco/falco_outputs_stdout.cpp
@@ -0,0 +1,39 @@
+/*
+Copyright (C) 2020 The Falco Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "falco_outputs_stdout.h"
+#include <iostream>
+#include "banned.h" // This raises a compilation error when certain functions are used
+
+void falco::outputs::output_stdout::output_event(gen_event *evt, std::string &rule, std::string &source,
+						 falco_common::priority_type priority, std::string &format, std::string &msg)
+{
+	output_msg(priority, msg);
+}
+
+void falco::outputs::output_stdout::output_msg(falco_common::priority_type priority, std::string &msg)
+{
+	std::cout << msg + "\n";
+	if(!m_buffered)
+	{
+		std::cout.flush();
+	}
+}
+
+void falco::outputs::output_stdout::cleanup()
+{
+	std::cout.flush();
+}
