b74d60289c30384f9a12f7abc7098dd4f509cb6d
Leonardo Grasso <me@leonardograsso.com>
2020-09-29 18:40:06 +0200

    chore: lua dir is not needed anymore in falco_outputs
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- userspace/falco/falco_outputs.h
+++ userspace/falco/falco_outputs.h
@@ -40,8 +40,7 @@ public:
 	void init(bool json_output,
 		  bool json_include_output_property,
 		  uint32_t rate, uint32_t max_burst, bool buffered,
-		  bool time_format_iso_8601, std::string hostname,
-		  const std::string& alternate_lua_dir);
+		  bool time_format_iso_8601, std::string hostname);
 
 	void add_output(falco::outputs::config oc);
 
