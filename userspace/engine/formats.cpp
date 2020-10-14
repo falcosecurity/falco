83f0d923577620efbf6dc245d68fbe44a9a44080
Leonardo Grasso <me@leonardograsso.com>
2020-10-14 17:08:25 +0200

    chore: update copyright year to 2020
    On previously modified files.
    
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- userspace/engine/formats.cpp
+++ userspace/engine/formats.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019 The Falco Authors.
+Copyright (C) 2020 The Falco Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
