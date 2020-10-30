458f7ccd3bf69c0b8eb7e62c67f12da8679b65f4
Leonardo Di Donato <leodidonato@gmail.com>
2020-10-30 13:30:25 +0000

    new(userspace): use conditional_variable to signal when we have a ready-to-use (with rules) falco engine
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- userspace/libhawk/hawk.h
+++ userspace/libhawk/hawk.h
@@ -3,9 +3,8 @@
 extern void hawk_init();
 extern void hawk_destroy();
 
-
 typedef void* hawk_engine;
-typedef void (*hawk_watch_rules_cb)(char *rules_content, hawk_engine* engine);
+typedef void (*hawk_watch_rules_cb)(char* rules_content, hawk_engine* engine);
 extern void hawk_watch_rules(hawk_watch_rules_cb cb, hawk_engine* engine);
 
 #endif //HAWK_H
