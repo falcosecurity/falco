77b8110770aa9a3b1dffde35b8261402094ec462
Leonardo Di Donato <leodidonato@gmail.com>
2020-11-10 13:24:11 +0000

    fix(scripts/ci-build-apline-aarch64.sh): -y flag not recognized by apk
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- scripts/ci-build-alpine-aarch64.sh
+++ scripts/ci-build-alpine-aarch64.sh
@@ -2,12 +2,12 @@
 
 set -xeu
 
-apk update -y
-apk add -y g++ gcc cmake cmake make ncurses-dev git bash perl linux-headers autoconf automake m4 libtool elfutils-dev libelf-static patch binutils
+apk update
+apk add g++ gcc cmake cmake make ncurses-dev git bash perl linux-headers autoconf automake m4 libtool elfutils-dev libelf-static patch binutils
 
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
-cmake -DUSE_BUNDLED_DEPS=On -DMUSL_OPTIMIZED_BUILD=On ..
+cmake -DUSE_BUNDLED_DEPS=On -DMUSL_OPTIMIZED_BUILD=On /falco
 make -j4 all
 make tests
 make -j4 package
