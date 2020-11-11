4507d6b43e69ddab81f656d4306e6f88ca875ce5
Leonardo Di Donato <leodidonato@gmail.com>
2020-11-11 10:07:59 +0000

    update(.circleci): split build steps
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- scripts/ci-build-alpine-aarch64.sh
+++ scripts/ci-build-alpine-aarch64.sh
@@ -2,13 +2,41 @@
 
 set -xeu
 
-apk update
-apk add g++ gcc cmake cmake make ncurses-dev git bash perl linux-headers autoconf automake m4 libtool elfutils-dev libelf-static patch binutils
+: "${BUILD_DIR?"Missing BUILD_DIR environment variable"}"
 
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
-cmake -DUSE_BUNDLED_DEPS=On -DMUSL_OPTIMIZED_BUILD=On -DBUILD_DRIVER=Off /falco
-make -j4 all
-make tests
-make -j4 package
 
+while test $# -gt 0; do
+    case "$1" in
+        prepare)
+            apk update
+            apk add g++ gcc cmake cmake make ncurses-dev git bash perl linux-headers autoconf automake m4 libtool elfutils-dev libelf-static patch binutils
+            exit 0
+            ;;
+        cmake)
+            cmake -DUSE_BUNDLED_DEPS=On -DMUSL_OPTIMIZED_BUILD=On -DBUILD_DRIVER=Off /falco
+            exit 0
+            ;;
+        falco)
+            make -j4 falco
+            exit 0
+            ;;
+        grpc)
+            make -j4 grpc
+            exit 0
+            ;;
+        all)
+            make -j4 all
+            exit 0
+            ;;
+        tests)
+            make tests
+            exit 0
+            ;;
+        packages)
+            make -j4 package
+            exit 0
+            ;;
+    esac
+done
