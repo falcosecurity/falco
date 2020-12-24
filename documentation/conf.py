eabd8b677bec445029e2ac9dfe5358f38b16f980
Leonardo Di Donato <leodidonato@gmail.com>
2020-12-24 15:26:23 +0000

    new(documentation): navbar config
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ documentation/conf.py
@@ -0,0 +1,19 @@
+DOXYFILE = 'Doxyfile'
+
+LINKS_NAVBAR1 = [
+    (None, 'pages', [(None, 'about')]),
+    (None, 'namespaces', []),
+]
+
+# Add your own navbar links using the code below.
+# To find the valid link names, you can inspect the URL of a generated documentation site.
+
+# LINKS_NAVBAR1 = [
+#     (None, 'pages', [(None, 'about')]),
+#     (None, 'namespaces', [(None, 'namespacexyz')]),
+# ]
+#
+# LINKS_NAVBAR2 = [
+#     (None, 'annotated', [(None, 'classxyz_1_1_xyz')]),
+#     (None, 'files', [(None, 'xyz_8h')]),
+# ]
