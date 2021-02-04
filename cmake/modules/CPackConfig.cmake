389060a2aa5ffde0a62d3d39ca818e02bfca8cde
JenTing Hsiao <jenting.hsiao@suse.com>
2021-02-04 14:38:51 +0800

    Bump year to 2021
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

--- cmake/modules/CPackConfig.cmake
+++ cmake/modules/CPackConfig.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The Falco Authors.
+# Copyright (C) 2021 The Falco Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
