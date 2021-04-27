25c5e5672cf7948fe6c052da6c95ca90a6708af9
Leonardo Grasso <me@leonardograsso.com>
2021-04-27 12:22:03 +0200

    build: correct yamlcpp dependency for falco
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- cmake/modules/yaml-cpp.cmake
+++ cmake/modules/yaml-cpp.cmake
@@ -28,6 +28,7 @@ else()
     yamlcpp
     URL "https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.2.tar.gz"
     URL_HASH "SHA256=e4d8560e163c3d875fd5d9e5542b5fd5bec810febdcba61481fe5fc4e6b1fd05"
+    BUILD_BYPRODUCTS ${YAMLCPP_LIB}
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND "")
 endif()
