8a9b7dfcfacfdd08ed832f9308bb87fe76f3787e
Mark Stemm <mark.stemm@gmail.com>
2021-10-06 15:27:13 -0700

    Squash w/ prior commit

--- cmake/modules/plugins.cmake
+++ cmake/modules/plugins.cmake
@@ -14,23 +14,18 @@
 include(ExternalProject)
 
 set(PLUGINS_PREFIX ${CMAKE_BINARY_DIR}/plugins-prefix)
-set(PLUGINS_DIR ${CMAKE_CURRENT_BINARY_DIR}/plugins)
-message(STATUS "Using bundled plugins in ${PLUGINS_DIR}")
-
-file(MAKE_DIRECTORY ${PLUGINS_DIR})
+set(PLUGINS_VERSION "0.1.0-rc1")
+string(TOLOWER ${CMAKE_SYSTEM_NAME} PLUGIN_SYSTEM_NAME)
+set(PLUGINS_FULL_VERSION "falcosecurity-plugins-${PLUGINS_VERSION}-${PLUGIN_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR}")
+message(STATUS "Using bundled plugins in ${PLUGINS_PREFIX}")
 
 ExternalProject_Add(
   plugins
   PREFIX ${PLUGINS_PREFIX}
-  GIT_REPOSITORY "https://github.com/falcosecurity/plugins.git"
-  GIT_TAG "0.1.0-rc1"
+  URL "https://download.falco.org/plugins/${PLUGINS_FULL_VERSION}.tar.gz"
+  URL_HASH "SHA256=3750b3e5120aba9c6d388f6bfdc3c150564edd21779876c3bcf7ec9d3afb66ad"
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND "make"
-  BUILD_IN_SOURCE 1
-  INSTALL_COMMAND
-    ${CMAKE_COMMAND} -E copy
-    ${PLUGINS_PREFIX}/src/plugins/plugins/cloudtrail/libcloudtrail.so
-    ${PLUGINS_PREFIX}/src/plugins/plugins/json/libjson.so
-    ${PLUGINS_DIR})
+  BUILD_COMMAND ""
+  INSTALL_COMMAND "")
 
-install(FILES "${PLUGINS_DIR}/libcloudtrail.so" "${PLUGINS_DIR}/libjson.so" DESTINATION "${FALCO_PLUGINS_DIR}")
+install(FILES "${PLUGINS_PREFIX}/src/plugins/cloudtrail/libcloudtrail.so" "${PLUGINS_PREFIX}/src/plugins/json/libjson.so" DESTINATION "${FALCO_PLUGINS_DIR}")
