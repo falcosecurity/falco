cbcc680c7707a0ed2ae8fa673c15789c610c988d
Mark Stemm <mark.stemm@gmail.com>
2022-02-16 17:32:54 -0800

    Cmake function to copy files from source to build dir, as a target
    Define a cmake function copy_files_to_build_dir(source_files,
    targetsuffix) that defines a single custom target and single custom
    command to copy the set of source files to
    CMAKE_CURRENT_BINARY_DIR. All of the real work is done via cmake -E
    copy_if_different.
    
    This function will replace the nearly identical cmake code in several
    other directories. This function has the advantage of being a single
    target for the set of source files instead of a target per-file.
    
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

---
+++ cmake/modules/copy_files_to_build_dir.cmake
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2022 The Falco Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
+function(copy_files_to_build_dir source_files targetsuffix)
+
+  set(build_files)
+
+  foreach(file_path ${source_files})
+	  get_filename_component(trace_file ${file_path} NAME)
+	  list(APPEND build_files ${CMAKE_CURRENT_BINARY_DIR}/${trace_file})
+  endforeach()
+
+  add_custom_target(copy-files-${targetsuffix} ALL
+	  DEPENDS ${build_files})
+
+  add_custom_command(OUTPUT ${build_files}
+	  COMMAND ${CMAKE_COMMAND} -E copy_if_different ${source_files} ${CMAKE_CURRENT_BINARY_DIR}
+	  DEPENDS ${source_files})
+
+endfunction()
