94824d9a6036c2828624b6c3f0c8e68c8f45eb1a
Mark Stemm <mark.stemm@gmail.com>
2022-03-22 14:26:02 -0700

    Application changes to support actions
    Changes to the falco::app::application object to support actions:
    
    - All of the code that was in falco_init is now in methods of
      application. (A later commit actually moves the code from falco_init
      and into the split-up methods, this commit just declares them).
    - Methods return an application::run_result object, which is a tuple
      of success/errstr/proceed. proceed=false is used to short circuit
      calling methods (think --help, --list, --support, etc.)
    - application now has a run() method which runs the methods in an order
      that honors any implicit dependencies (e.g. you have to init an
      inspector before you open it, you have to do almost everything else
      before processing events, etc.)
    - There are a few methods devoted to teardown, they are always called
      after the group of run methods are called.
    - State that needs to be saved between methods, or saved between the
      run and teardown functions, is in a
      falco::app::application::state object.
    
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

--- userspace/falco/application.cpp
+++ userspace/falco/application.cpp
@@ -24,8 +24,39 @@ limitations under the License.
 #include "application.h"
 #include "falco_common.h"
 
+using namespace std::placeholders;
+
 namespace falco {
 namespace app {
+
+std::string application::s_syscall_source = falco_common::syscall_source;
+std::string application::s_k8s_audit_source = "k8s_audit";
+
+application::run_result::run_result()
+	: success(true), errstr(""), proceed(true)
+{
+}
+
+application::run_result::~run_result()
+{
+}
+
+application::state::state()
+	: restart(false),
+	  terminate(false),
+	  reopen_outputs(false),
+	  enabled_sources({application::s_syscall_source, application::s_k8s_audit_source}),
+	  trace_is_scap(true)
+{
+	config = std::make_shared<falco_configuration>();
+	outputs = std::make_shared<falco_outputs>();
+	engine = std::make_shared<falco_engine>();
+	inspector = std::make_shared<sinsp>();
+}
+
+application::state::~state()
+{
+}
 
 application::application()
 	: m_initialized(false)
@@ -36,25 +67,128 @@ application::~application()
 {
 }
 
-cmdline_options &application::options()
+void application::terminate()
 {
-	if(!m_initialized)
+	if(m_state != nullptr)
 	{
-		throw falco_exception("App init() not called yet");
+		m_state->terminate = true;
 	}
+}
 
-	return m_cmdline_options;
+void application::reopen_outputs()
+{
+	if(m_state != nullptr)
+	{
+		m_state->reopen_outputs = true;
+	}
+}
+
+void application::restart()
+{
+	if(m_state != nullptr)
+	{
+		m_state->restart = true;
+	}
 }
 
 bool application::init(int argc, char **argv, std::string &errstr)
 {
-	if(!m_cmdline_options.parse(argc, argv, errstr))
+	if(m_initialized)
+	{
+		throw falco_exception("Application already initialized");
+	}
+
+	m_state.reset(new state());
+
+	if(!m_options.parse(argc, argv, errstr))
 	{
 		return false;
 	}
 
+	for(char **arg = argv; *arg; arg++)
+	{
+		if(m_state->cmdline.size() > 0)
+		{
+			m_state->cmdline += " ";
+		}
+		m_state->cmdline += *arg;
+	}
+
 	m_initialized = true;
 	return true;
+}
+
+bool application::run(std::string &errstr, bool &restart)
+{
+	run_result res;
+
+	// The order here is the order in which the methods will be
+	// called. Before changing the order, ensure that all
+	// dependencies are honored (e.g. don't process events before
+	// loading plugins, opening inspector, etc.).
+	std::list<std::function<run_result()>> run_steps = {
+		std::bind(&application::print_help, this),
+		std::bind(&application::print_version, this),
+		std::bind(&application::create_signal_handlers, this),
+		std::bind(&application::load_config, this),
+		std::bind(&application::init_inspector, this),
+		std::bind(&application::init_falco_engine, this),
+		std::bind(&application::load_plugins, this),
+		std::bind(&application::list_fields, this),
+		std::bind(&application::list_plugins, this),
+		std::bind(&application::load_rules_files, this),
+		std::bind(&application::print_ignored_events, this),
+		std::bind(&application::print_support, this),
+		std::bind(&application::validate_rules_files, this),
+		std::bind(&application::daemonize, this),
+		std::bind(&application::init_outputs, this),
+		std::bind(&application::open_inspector, this),
+#ifndef MINIMAL_BUILD
+		std::bind(&application::start_grpc_server, this),
+		std::bind(&application::start_webserver, this),
+#endif
+		std::bind(&application::process_events, this)
+	};
+
+	std::list<std::function<bool(std::string &)>> teardown_steps = {
+		std::bind(&application::close_inspector, this, _1),
+		std::bind(&application::unregister_signal_handlers, this, _1),
+#ifndef MINIMAL_BUILD
+		std::bind(&application::stop_grpc_server, this, _1),
+		std::bind(&application::stop_webserver, this, _1)
+#endif
+	};
+
+	for (auto &func : run_steps)
+	{
+		res = func();
+
+		if(!res.proceed)
+		{
+			break;
+		}
+	}
+
+	for (auto &func : teardown_steps)
+	{
+		std::string errstr;
+
+		if(!func(errstr))
+		{
+			// Note only printing warning here--we want all functions
+			// to occur even if some return errors.
+			fprintf(stderr, "Could not tear down in run(): %s\n", errstr.c_str());
+		}
+	}
+
+	if(!res.success)
+	{
+		errstr = res.errstr;
+	}
+
+	restart = m_state->restart;
+
+	return res.success;
 }
 
 }; // namespace app
