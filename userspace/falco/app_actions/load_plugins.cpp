9b17d4ead59c8020614f67475abe48a25db0a498
Federico Di Pierro <nierro92@gmail.com>
2022-04-01 17:56:13 +0200

    new(userspace/falco): use new plugin caps API.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- userspace/falco/app_actions/load_plugins.cpp
+++ userspace/falco/app_actions/load_plugins.cpp
@@ -40,7 +40,7 @@ application::run_result application::load_plugins()
 	}
 
 	std::shared_ptr<sinsp_plugin> input_plugin;
-	std::list<std::shared_ptr<sinsp_plugin>> extractor_plugins;
+	std::list<std::shared_ptr<sinsp_plugin_cap_extraction>> extractor_plugins;
 	for(auto &p : m_state->config->m_plugins)
 	{
 		std::shared_ptr<sinsp_plugin> plugin;
@@ -52,16 +52,13 @@ application::run_result application::load_plugins()
 		falco_logger::log(LOG_INFO, "Loading plugin (" + p.m_name + ") from file " + p.m_library_path + "\n");
 
 		// libs requires raw pointer, we should modify libs to use reference/shared_ptr
-		plugin = sinsp_plugin::register_plugin(m_state->inspector.get(),
-						       p.m_library_path,
-						       (p.m_init_config.empty() ? NULL : (char *)p.m_init_config.c_str()),
-						       m_state->plugin_filter_checks);
+		plugin = m_state->inspector->register_plugin(p.m_library_path,
+						    (p.m_init_config.empty() ? nullptr : (char *)p.m_init_config.c_str()),
+							     m_state->plugin_filter_checks);
 #endif
 
-		if(plugin->type() == TYPE_SOURCE_PLUGIN)
+		if(plugin->caps() & CAP_SOURCING)
 		{
-			sinsp_source_plugin *splugin = static_cast<sinsp_source_plugin *>(plugin.get());
-
 			if(input_plugin)
 			{
 				ret.success = false;
@@ -71,17 +68,20 @@ application::run_result application::load_plugins()
 			}
 
 			input_plugin = plugin;
-			event_source = splugin->event_source();
+			event_source = plugin->event_source();
 
 			m_state->inspector->set_input_plugin(p.m_name);
 			if(!p.m_open_params.empty())
 			{
-				m_state->inspector->set_input_plugin_open_params(p.m_open_params.c_str());
+				m_state->inspector->set_input_plugin_open_params(p.m_open_params);
 			}
 
 			m_state->event_source_idx = m_state->engine->add_source(event_source, plugin_filter_factory, plugin_formatter_factory);
 
-		} else {
+		}
+
+		if(plugin->caps() & CAP_EXTRACTION)
+		{
 			extractor_plugins.push_back(plugin);
 		}
 	}
@@ -89,12 +89,11 @@ application::run_result application::load_plugins()
 	// Ensure that extractor plugins are compatible with the event source.
 	// Also, ensure that extractor plugins don't have overlapping compatible event sources.
 	std::set<std::string> compat_sources_seen;
-	for(auto plugin : extractor_plugins)
+	for(const auto& eplugin : extractor_plugins)
 	{
 		// If the extractor plugin names compatible sources,
 		// ensure that the input plugin's source is in the list
 		// of compatible sources.
-		sinsp_extractor_plugin *eplugin = static_cast<sinsp_extractor_plugin *>(plugin.get());
 		const std::set<std::string> &compat_sources = eplugin->extract_event_sources();
 		if(input_plugin &&
 		   !compat_sources.empty())
@@ -119,7 +118,7 @@ application::run_result application::load_plugins()
 		}
 	}
 
-	m_state->plugin_infos = sinsp_plugin::plugin_infos(m_state->inspector.get());
+	m_state->plugin_infos = m_state->inspector->plugin_infos();
 
 	return ret;
 }
