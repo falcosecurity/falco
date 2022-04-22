0cbd5281c7dcaaf381bb200e6a93a1b00d6d3bd1
Jason Dellaluce <jasondellaluce@gmail.com>
2022-04-22 07:49:39 +0000

    refactor: add k8saudit plugin and adapt config, tests, and rulesets
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/engine/rule_loader.cpp
+++ userspace/engine/rule_loader.cpp
@@ -724,6 +724,7 @@ void rule_loader::compile_rule_infos(
 			set<uint16_t> evttypes = { ppm_event_type::PPME_PLUGINEVENT_E };
 			if(rule.source == falco_common::syscall_source)
 			{
+				evttypes.clear();
 				filter_evttype_resolver().evttypes(ast, evttypes);
 				if ((evttypes.empty() || evttypes.size() > 100)
 					&& r.warn_evttypes)
