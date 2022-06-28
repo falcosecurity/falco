2caadd1af523ffd93585b919a6827153550d4a88
Jason Dellaluce <jasondellaluce@gmail.com>
2022-06-28 12:14:16 +0000

    refactor(userspace/falco): add action for printing syscall events
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/falco/app_actions/print_syscall_events.cpp
+++ userspace/falco/app_actions/print_syscall_events.cpp
@@ -14,22 +14,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "fields_info.h"
-
 #include "application.h"
+
+#include <fields_info.h>
 
 using namespace falco::app;
 
-application::run_result application::list_fields()
+application::run_result application::print_syscall_events()
 {
-	if(m_options.list_fields)
+	if(m_options.list_syscall_events)
 	{
-		if(m_options.list_source_fields != "" &&
-		   !m_state->engine->is_source_valid(m_options.list_source_fields))
-		{
-			return run_result::fatal("Value for --list must be a valid source type");
-		}
-		m_state->engine->list_fields(m_options.list_source_fields, m_options.verbose, m_options.names_only, m_options.markdown);
+		// We know this function doesn't hold into the raw pointer value
+		std::unique_ptr<sinsp> inspector(new sinsp());
+		list_events(inspector.get(), m_options.markdown);
 		return run_result::exit();
 	}
 
