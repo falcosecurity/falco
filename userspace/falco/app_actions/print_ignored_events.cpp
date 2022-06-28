44d5103c808a5606690678a035e9eb4f48791684
Jason Dellaluce <jasondellaluce@gmail.com>
2022-06-28 12:17:09 +0000

    refactor(userspace/falco): drop inspector dependency on print_ignored_events action
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/falco/app_actions/print_ignored_events.cpp
+++ userspace/falco/app_actions/print_ignored_events.cpp
@@ -20,7 +20,8 @@ using namespace falco::app;
 
 void application::print_all_ignored_events()
 {
-	sinsp_evttables* einfo = m_state->inspector->get_event_info_tables();
+	std::unique_ptr<sinsp> inspector(new sinsp());
+	sinsp_evttables* einfo = inspector->get_event_info_tables();
 	const struct ppm_event_info* etable = einfo->m_event_info;
 	const struct ppm_syscall_desc* stable = einfo->m_syscall_info_table;
 
