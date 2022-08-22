db92f04474772899f28291f4898563f78e5fe546
Federico Di Pierro <nierro92@gmail.com>
2022-08-22 15:09:36 +0200

    wip
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/driver.cmake
+++ cmake/modules/driver.cmake
@@ -26,8 +26,8 @@ else()
   # In case you want to test against another driver version (or branch, or commit) just pass the variable -
   # ie., `cmake -DDRIVER_VERSION=dev ..`
   if(NOT DRIVER_VERSION)
-    set(DRIVER_VERSION "b4c198773bf05486e122f6d3f7f63be125242413")
-    set(DRIVER_CHECKSUM "SHA256=e85fa42a0b58ba21ca7efb38c20ce25207f4816245bdf154e6b9a037a1cce930")
+    set(DRIVER_VERSION "d673b2722e8a730013bfe525eaa417945ef8b723")
+    set(DRIVER_CHECKSUM "SHA256=de7a2fb42da781d5b867f25a009cfdf8b03a1c12dc9a5d2abba08c7afb5a14d4")
   endif()
 
   # cd /path/to/build && cmake /path/to/source
