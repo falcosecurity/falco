be75d44e8f864b3f0ebe7db71074b09d1956a82d
Jason Dellaluce <jasondellaluce@gmail.com>
2023-01-12 17:38:51 +0000

    fix(userspace/falco): solve tests issues
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/falco/application.cpp
+++ userspace/falco/application.cpp
@@ -159,8 +159,6 @@ bool application::init(int argc, char **argv, std::string &errstr)
 		m_state->cmdline += *arg;
 	}
 
-	// initialize default config with cmdline overrides (-o option)
-	m_state->config->init(m_options.cmdline_config_options);
 	m_initialized = true;
 	return true;
 }
