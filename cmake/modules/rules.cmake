7c4f1de10a29b028998aa750151fca523ca356a7
Andrea Terzolo <andrea.terzolo@polito.it>
2023-01-25 13:00:34 +0100

    chore: bump rules and falcoctl
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- cmake/modules/rules.cmake
+++ cmake/modules/rules.cmake
@@ -16,11 +16,11 @@ include(ExternalProject)
 
 # falco_rules.yaml
 set(FALCOSECURITY_RULES_FALCO_VERSION "falco-rules-0.1.0")
-set(FALCOSECURITY_RULES_FALCO_CHECKSUM "SHA256=f909a996213ab659ef9f958ae64b16c83dca0035525cc044d4dc074250e8b152")
-set(FALCOSECURITY_RULES_FALCO_PATH "${PROJECT_BINARY_DIR}/falcosecurity-rules-falco-prefix/src/falcosecurity-rules-falco/rules/falco_rules.yaml")
+set(FALCOSECURITY_RULES_FALCO_CHECKSUM "SHA256=0d3705a4650f09d10e7831b16e7af59c1da34ff19e788896e9ee77010014db4d")
+set(FALCOSECURITY_RULES_FALCO_PATH "${PROJECT_BINARY_DIR}/falcosecurity-rules-falco-prefix/src/falcosecurity-rules-falco/falco_rules.yaml")
 ExternalProject_Add(
   falcosecurity-rules-falco
-  URL "https://github.com/falcosecurity/rules/archive/${FALCOSECURITY_RULES_FALCO_VERSION}.tar.gz"
+  URL "https://download.falco.org/rules/${FALCOSECURITY_RULES_FALCO_VERSION}.tar.gz"
   URL_HASH "${FALCOSECURITY_RULES_FALCO_CHECKSUM}"
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
@@ -30,11 +30,11 @@ ExternalProject_Add(
 
 # application_rules.yaml
 set(FALCOSECURITY_RULES_APPLICATION_VERSION "application-rules-0.1.0")
-set(FALCOSECURITY_RULES_APPLICATION_CHECKSUM "SHA256=33fcdc121cedb8e49c735c7948ede7d9427fd61962914fc3b66fe3d94d331cbf")
-set(FALCOSECURITY_RULES_APPLICATION_PATH "${PROJECT_BINARY_DIR}/falcosecurity-rules-application-prefix/src/falcosecurity-rules-application/rules/application_rules.yaml")
+set(FALCOSECURITY_RULES_APPLICATION_CHECKSUM "SHA256=cf45c1a6997799610a7724ba7a2ceaa64a3bdc73d26cdfe06adb3f43e2321278")
+set(FALCOSECURITY_RULES_APPLICATION_PATH "${PROJECT_BINARY_DIR}/falcosecurity-rules-application-prefix/src/falcosecurity-rules-application/application_rules.yaml")
 ExternalProject_Add(
   falcosecurity-rules-application
-  URL "https://github.com/falcosecurity/rules/archive/${FALCOSECURITY_RULES_APPLICATION_VERSION}.tar.gz"
+  URL "https://download.falco.org/rules/${FALCOSECURITY_RULES_APPLICATION_VERSION}.tar.gz"
   URL_HASH "${FALCOSECURITY_RULES_APPLICATION_CHECKSUM}"
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
