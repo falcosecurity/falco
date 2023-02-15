4629f63d33a7dd3ff8e280729a99f8d05d302157
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-02-15 07:42:37 +0000

    fix rebase
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/falco/app/actions/helpers_interesting_sets.cpp
+++ userspace/falco/app/actions/helpers_interesting_sets.cpp
@@ -17,7 +17,6 @@ limitations under the License.
 
 #include "helpers.h"
 #include "actions.h"
-#include "configure_interesting_sets.h"
 #include "falco_utils.h"
 #include <unordered_set>
 #include <sinsp.h>
