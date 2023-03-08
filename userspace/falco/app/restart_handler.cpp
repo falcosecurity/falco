e07e3abfb5cb899c7adc026677022d100428219a
Jason Dellaluce <jasondellaluce@gmail.com>
2023-03-08 18:33:44 +0000

    update(userspace/falco): implement debouncing logic in restart handler
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/falco/app/restart_handler.cpp
+++ userspace/falco/app/restart_handler.cpp
@@ -89,18 +89,24 @@ void falco::app::restart_handler::watcher_loop() noexcept
 {
     if (fcntl(m_inotify_fd, F_SETOWN, gettid()) < 0)
     {
-        falco_logger::log(LOG_ERR, "Failed setting owner on inotify handler");
+        // an error occurred, we can't recover
+        // todo(jasondellaluce): should we terminate the process?
+        falco_logger::log(LOG_ERR, "Failed owning inotify handler, shutting down watcher...");
         return;
     }
 
     fd_set set;
     bool forced = false;
+    bool should_check = false;
     bool should_restart = false;
     struct timeval timeout;
     uint8_t buf[(10 * (sizeof(struct inotify_event) + NAME_MAX + 1))];
     while (!m_stop.load(std::memory_order_acquire))
     {
-        // wait for inotify events with a certain timeout
+        // wait for inotify events with a certain timeout.
+        // Note, we'll run through select even before performing a dry-run,
+        // so that we can dismiss in case we have to debounce rapid
+        // subsequent events.
         timeout.tv_sec = 0;
         timeout.tv_usec = 100000;
         FD_ZERO(&set);
@@ -108,7 +114,9 @@ void falco::app::restart_handler::watcher_loop() noexcept
         auto rv = select(m_inotify_fd + 1, &set, NULL, NULL, &timeout);
         if (rv < 0)
         {
-            falco_logger::log(LOG_ERR, "Failed select in inotify watcher");
+            // an error occurred, we can't recover
+            // todo(jasondellaluce): should we terminate the process?
+            falco_logger::log(LOG_ERR, "Failed select with inotify handler, shutting down watcher...");
             return;
         }
         
@@ -116,45 +124,81 @@ void falco::app::restart_handler::watcher_loop() noexcept
         forced = m_forced.load(std::memory_order_acquire);
         m_forced.store(false, std::memory_order_release);
 
-        if (rv > 0 || forced)
+        // no new watch event is received during the timeout
+        if (rv == 0 && !forced)
         {
-            // if new inotify events have been received during the previous
-            // dry run, even if the dry run was successful, we dismiss
-            // the restart attempt and perform an additional dry-run for
-            // safety purpose (the new inotify events may be related to
-            // bad config/rules files changes).
-            should_restart = false;
-
-            // if there's date on the inotify fd, consume it
-            if (rv > 0)
+            // perform a dry run. In case no error occurrs, we loop back
+            // to the select in order to debounce new inotify events before
+            // actually triggering a restart.
+            if (should_check)
             {
-                auto n = read(m_inotify_fd, buf, sizeof(buf));
-                if (n == 0)
+                should_check = false;
+                should_restart = m_on_check();
+                continue;
+            }
+
+            // if the previous dry run was successful, and no new
+            // inotify events have been received during the dry run,
+            // then we trigger the restarting signal and quit.
+            // note: quitting is a time optimization, the thread
+            // will be forced to quit anyways later by the Falco app, but
+            // at least we don't make users wait for the timeout.
+            if (should_restart)
+            {
+                should_restart = false;
+                // todo(jasondellaluce): make this a callback too maybe?
+                g_restart_signal.trigger();
+                return;
+            }
+
+            // let's go back to the select
+            continue;
+        }
+
+        // at this point, we either received a new inotify event or a forced
+        // restart. If this happened during a dry run (even if the dry run
+        // was successful), or during a timeout wait since the last successful
+        // dry run before a restart, we dismiss the restart attempt and
+        // perform an additional dry-run for safety purposes (the new inotify
+        // events may be related to bad config/rules files changes).
+        should_restart = false;
+        should_check = false;
+
+        // if there's date on the inotify fd, consume it
+        // (even if there is a forced request too)
+        if (rv > 0)
+        {
+            // note: if available data is less than buffer size, this should
+            // return n > 0 but not filling the buffer. If available data is
+            // more than buffer size, we will loop back to select and behave
+            // like we debounced an event.
+            auto n = read(m_inotify_fd, buf, sizeof(buf));
+            if (n < 0)
+            {
+                // an error occurred, we can't recover
+                // todo(jasondellaluce): should we terminate the process?
+                falco_logger::log(LOG_ERR, "Failed read with inotify handler, shutting down watcher...");
+                return;
+            }
+            // this is an odd case, but if we got here with
+            // no read data, and no forced request, we get back
+            // looping in the select. This can likely happen if
+            // there's data in the inotify fd but the first read
+            // returned no bytes. Likely we'll get back here at the
+            // next select call.
+            else if (n == 0)
+            {
+                // we still proceed in case the request was forced
+                if (!forced)
                 {
                     continue;
                 }
-                if (n < 0)
-                {
-                    falco_logger::log(LOG_ERR, "Failed read in inotify watcher");
-                    return;
-                }
-            }
-
-            // perform a check run check and attempt restarting
-            if (m_on_check())
-            {
-                should_restart = true;
             }
         }
 
-        // if the previous dry run was successful, and no new
-        // inotify events have been received during the dry run,
-        // then we trigger the restarting signal and quit
-        if (should_restart)
-        {
-            // todo(jasondellaluce): make this a callback too maybe?
-            g_restart_signal.trigger();
-            return;
-        }
+        // we consumed the new inotify events or we received a forced
+        // restart request, so we'll perform a dry run after the
+        // next timeout.
+        should_check = true;
     }
 }
