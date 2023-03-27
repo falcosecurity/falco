b2e03b1938569ba7e881bb3ec721a267c77511f4
Federico Di Pierro <nierro92@gmail.com>
2023-03-27 18:03:12 +0200

    chore(userspace): `syscall_drop_failed` -> `syscall_drop_failed_exit`.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>
    
    Co-authored-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/falco/configuration.cpp
+++ userspace/falco/configuration.cpp
@@ -58,7 +58,7 @@ falco_configuration::falco_configuration():
 	m_metadata_download_watch_freq_sec(1),
 	m_syscall_buf_size_preset(4),
 	m_cpus_for_each_syscall_buffer(2),
-	m_syscall_drop_failed(false)
+	m_syscall_drop_failed_exit(false)
 {
 }
 
@@ -314,7 +314,7 @@ void falco_configuration::load_yaml(const std::string& config_name, const yaml_h
 
 	m_cpus_for_each_syscall_buffer = config.get_scalar<uint16_t>("modern_bpf.cpus_for_each_syscall_buffer", 2);
 
-	m_syscall_drop_failed = config.get_scalar<bool>("syscall_drop_failed", false);
+	m_syscall_drop_failed_exit = config.get_scalar<bool>("syscall_drop_failed_exit", false);
 
 	m_base_syscalls.clear();
 	config.get_sequence<std::unordered_set<std::string>>(m_base_syscalls, std::string("base_syscalls"));
