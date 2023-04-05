e10d54df39201fc9ea524b461495d4eeb58df4e8
0xsh4dy <rakshitawasthi17@gmail.com>
2023-04-05 22:47:30 +0530

    Set default mode to bpf probe

--- userspace/falco/app/actions/helpers_inspector.cpp
+++ userspace/falco/app/actions/helpers_inspector.cpp
@@ -17,14 +17,14 @@ limitations under the License.
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include<fstream>
 #include <plugin_manager.h>
 
 #include "helpers.h"
 
 /* DEPRECATED: we will remove it in Falco 0.34. */
 #define FALCO_BPF_ENV_VARIABLE "FALCO_BPF_PROBE"
-
+#define BPF_PROBE_PATH "/etc/clouddefense/probe.o"
 using namespace falco::app;
 using namespace falco::app::actions;
 
@@ -49,76 +49,15 @@ falco::app::run_result falco::app::actions::open_live_inspector(
 {
 	try
 	{
-		if (source != falco_common::syscall_source) /* Plugin engine */
-		{
-			for (const auto& p: inspector->get_plugin_manager()->plugins())
-			{
-				if (p->caps() & CAP_SOURCING && p->event_source() == source)
-				{
-					auto cfg = s.plugin_configs.at(p->name());
-					falco_logger::log(LOG_INFO, "Opening capture with plugin '" + cfg->m_name + "'\n");
-					inspector->open_plugin(cfg->m_name, cfg->m_open_params);
-					return run_result::ok();
-				}
-			}
-			return run_result::fatal("Can't open inspector for plugin event source: " + source);
+		std::ifstream bpf_probe(BPF_PROBE_PATH);
+		if(bpf_probe.good()){
+			falco_logger::log(LOG_INFO,"Opening capture with BPF probe");
+			inspector->open_bpf(BPF_PROBE_PATH,s.syscall_buffer_bytes_size,s.selected_sc_set);
 		}
-		else if (s.options.userspace) /* udig engine. */
-		{
-			// open_udig() is the underlying method used in the capture code to parse userspace events from the kernel.
-			//
-			// Falco uses a ptrace(2) based userspace implementation.
-			// Regardless of the implementation, the underlying method remains the same.
-			falco_logger::log(LOG_INFO, "Opening capture with udig\n");
-			inspector->open_udig();
+		else{
+			return run_result::fatal("Error! BPF probe /etc/clouddefense/probe.o not found");
 		}
-		else if(!s.options.gvisor_config.empty()) /* gvisor engine. */
-		{
-			falco_logger::log(LOG_INFO, "Opening capture with gVisor. Configuration path: " + s.options.gvisor_config);
-			inspector->open_gvisor(s.options.gvisor_config, s.options.gvisor_root);
-		}
-		else if(s.options.modern_bpf) /* modern BPF engine. */
-		{
-			falco_logger::log(LOG_INFO, "Opening capture with modern BPF probe.");
-			falco_logger::log(LOG_INFO, "One ring buffer every '" + std::to_string(s.config->m_cpus_for_each_syscall_buffer) +  "' CPUs.");
-			inspector->open_modern_bpf(s.syscall_buffer_bytes_size, s.config->m_cpus_for_each_syscall_buffer, true, s.selected_sc_set);
-		}
-		else if(getenv(FALCO_BPF_ENV_VARIABLE) != NULL) /* BPF engine. */
-		{
-			const char *bpf_probe_path = std::getenv(FALCO_BPF_ENV_VARIABLE);
-			char full_path[PATH_MAX];
-			/* If the path is empty try to load the probe from the default path. */
-			if(strncmp(bpf_probe_path, "", 1) == 0)
-			{
-				const char *home = std::getenv("HOME");
-				if(!home)
-				{
-					return run_result::fatal("Cannot get the env variable 'HOME'");
-				}
-				snprintf(full_path, PATH_MAX, "%s/%s", home, FALCO_PROBE_BPF_FILEPATH);
-				bpf_probe_path = full_path;
-			}
-			falco_logger::log(LOG_INFO, "Opening capture with BPF probe. BPF probe path: " + std::string(bpf_probe_path));
-			inspector->open_bpf(bpf_probe_path, s.syscall_buffer_bytes_size, s.selected_sc_set);
-		}
-		else /* Kernel module (default). */
-		{
-			try
-			{
-				falco_logger::log(LOG_INFO, "Opening capture with Kernel module");
-				inspector->open_kmod(s.syscall_buffer_bytes_size, s.selected_sc_set);
-			}
-			catch(sinsp_exception &e)
-			{
-				// Try to insert the Falco kernel module
-				falco_logger::log(LOG_INFO, "Trying to inject the Kernel module and opening the capture again...");
-				if(system("modprobe " DRIVER_NAME " > /dev/null 2> /dev/null"))
-				{
-					falco_logger::log(LOG_ERR, "Unable to load the driver\n");
-				}
-				inspector->open_kmod(s.syscall_buffer_bytes_size, s.selected_sc_set);
-			}
-		}
+
 	}
 	catch (sinsp_exception &e)
 	{
