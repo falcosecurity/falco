f2318a9ac5099d307a2aa0cf087ab1ae1d6bf52d
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-05-20 16:14:03 +0000

    cleanup(userspace/falco): address reviewers comments + cleanup
    * prefix counters and stats belonging to kernel space w/ `k.` else `u.` for userspace
    * add n_drops_perc from old stats writer schema
    * revert one change: file output shall reflect exact same "output_fields" key as rule output, note that src is already part of the "output_fields" schema.
    
    Co-authored-by: Jason Dellaluce <jasondellaluce@gmail.com>
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/engine/banned.h
+++ userspace/engine/banned.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019 The Falco Authors.
+Copyright (C) 2023 The Falco Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -36,9 +36,6 @@ limitations under the License.
 
 #undef strcat
 #define strcat(a, b) BAN(strcat)
-
-#undef strncat
-#define strncat(a, b, c) BAN(strncat)
 
 #undef strncpy
 #define strncpy(a, b, c) BAN(strncpy)
