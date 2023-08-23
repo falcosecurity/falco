9a12a93342aaade50105a3ec3aee3a29ed03c2f7
Melissa Kilby <melissa.kilby.oss@gmail.com>
2023-08-23 22:57:19 +0000

    feat(userspace): deprecate stats command args option in favor of metrics configs in falco.yaml
    Signed-off-by: Melissa Kilby <melissa.kilby.oss@gmail.com>

--- userspace/falco/app/actions/load_config.cpp
+++ userspace/falco/app/actions/load_config.cpp
@@ -25,20 +25,7 @@ static void apply_deprecated_options(
 		const falco::app::options& opts,
 		const std::shared_ptr<falco_configuration>& cfg)
 {
-	if (!opts.stats_output_file.empty() || !opts.stats_interval.empty())
-	{
-		falco_logger::log(LOG_WARNING, "Options '-s' and '--stats-interval' will be deprecated in the future, metrics must be configured through config file");
-		if (!opts.stats_output_file.empty())
-		{
-			cfg->m_metrics_enabled = true;
-			cfg->m_metrics_output_file = opts.stats_output_file;
-			if (!opts.stats_interval.empty())
-			{
-				cfg->m_metrics_interval_str = opts.stats_interval;
-				cfg->m_metrics_interval = falco::utils::parse_prometheus_interval(cfg->m_metrics_interval_str);
-			}
-		}
-	}
+	// Keep for future use cases.
 }
 
 falco::app::run_result falco::app::actions::load_config(falco::app::state& s)
