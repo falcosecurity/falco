38514314d1713a7c10734b8549ac1b00715ba26f
Andrea Terzolo <andreaterzolo3@gmail.com>
2023-08-29 12:47:55 +0200

    cleanup: turn a warning into an error
    Signed-off-by: Andrea Terzolo <andreaterzolo3@gmail.com>

--- userspace/falco/app/actions/process_events.cpp
+++ userspace/falco/app/actions/process_events.cpp
@@ -432,7 +432,7 @@ static falco::app::run_result init_stats_writer(
 
 	if (config->m_metrics_enabled && !sw->has_output())
 	{
-		falco_logger::log(LOG_WARNING, "Metrics are enabled with no output configured, no snapshot will be collected");
+		return falco::app::run_result::fatal("Metrics are enabled with no output configured. Please enable at least one output channel");
 	}
 
 	falco_logger::log(LOG_INFO, "Setting metrics interval to " + config->m_metrics_interval_str + ", equivalent to " + std::to_string(config->m_metrics_interval) + " (ms)\n");
