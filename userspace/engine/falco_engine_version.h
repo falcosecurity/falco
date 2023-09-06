dfeb056152cf874c362d0699b2d522e43fabf0e4
Luca Guerra <luca@guerra.sh>
2023-09-06 12:41:18 +0000

    update(falco)!: --list-syscall-events is now called --list-events
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- userspace/engine/falco_engine_version.h
+++ userspace/engine/falco_engine_version.h
@@ -19,7 +19,7 @@ limitations under the License.
 
 // This is the result of running the following command:
 //   FALCO="falco -c ./falco.yaml"
-//   echo $($FALCO --version | grep 'Engine:' | awk '{print $2}') $(echo $($FALCO --version | grep 'Schema version:' | awk '{print $3}') $($FALCO --list --markdown | grep '^`' | sort) $($FALCO --list-syscall-events | sort) | sha256sum)
+//   echo $($FALCO --version | grep 'Engine:' | awk '{print $2}') $(echo $($FALCO --version | grep 'Schema version:' | awk '{print $3}') $($FALCO --list --markdown | grep '^`' | sort) $($FALCO --list-events | sort) | sha256sum)
 // It represents the fields supported by this version of Falco,
 // the event types, and the underlying driverevent schema. It's used to
 // detetect changes in engine version in our CI jobs.
