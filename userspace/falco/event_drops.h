6d9513d3c0d0f90d5b4746212b11375cedf87d5b
Leonardo Grasso <me@leonardograsso.com>
2023-09-14 17:28:09 +0200

    update: add SPDX license identifier
    See https://github.com/falcosecurity/evolution/issues/318
    
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- userspace/falco/event_drops.h
+++ userspace/falco/event_drops.h
@@ -1,5 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
 /*
-Copyright (C) 2021 The Falco Authors.
+Copyright (C) 2023 The Falco Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
