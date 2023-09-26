45c59fe5273dd923e39365fa58a842ce991e498c
Andrea Terzolo <andreaterzolo3@gmail.com>
2023-09-26 14:16:18 +0200

    cleanup(falco)!: remove `--userspace` support
    Signed-off-by: Andrea Terzolo <andreaterzolo3@gmail.com>

--- userspace/falco/stats_writer.cpp
+++ userspace/falco/stats_writer.cpp
@@ -229,7 +229,7 @@ void stats_writer::collector::get_metrics_output_fields_wrapper(
 {
 	static const char* all_driver_engines[] = {
 		BPF_ENGINE, KMOD_ENGINE, MODERN_BPF_ENGINE,
-		SOURCE_PLUGIN_ENGINE, NODRIVER_ENGINE, UDIG_ENGINE, GVISOR_ENGINE };
+		SOURCE_PLUGIN_ENGINE, NODRIVER_ENGINE, GVISOR_ENGINE };
 	const scap_agent_info* agent_info = inspector->get_agent_info();
 	const scap_machine_info* machine_info = inspector->get_machine_info();
 
