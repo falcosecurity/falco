9bbf9716b6b56bb7025dffed22d213b4f0d32abf
Lorenzo Susini <susinilorenzo1@gmail.com>
2023-09-26 10:23:42 +0000

    update(userspace/falco): engine version semver in protobuf and versions_info
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- userspace/falco/grpc_server_impl.cpp
+++ userspace/falco/grpc_server_impl.cpp
@@ -79,6 +79,10 @@ void falco::grpc::server_impl::version(const context& ctx, const version::reques
 
 	res.set_engine_version(FALCO_ENGINE_VERSION);
 	res.set_engine_fields_checksum(FALCO_ENGINE_CHECKSUM); 
+	auto engine_version = sinsp_version(FALCO_ENGINE_VERSION);
+	res.set_engine_major(engine_version.m_version_major);
+	res.set_engine_minor(engine_version.m_version_minor);
+	res.set_engine_patch(engine_version.m_version_patch);
 
 	res.set_major(FALCO_VERSION_MAJOR);
 	res.set_minor(FALCO_VERSION_MINOR);
