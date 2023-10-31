029b3f6a3a07e89cf202d7104c5f86099b5449e3
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-10-31 10:20:37 +0000

    fix(userspace/falco): include windows.h in print actions
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- userspace/falco/app/actions/print_page_size.cpp
+++ userspace/falco/app/actions/print_page_size.cpp
@@ -16,6 +16,10 @@ limitations under the License.
 */
 
 #include "actions.h"
+#ifdef _WIN32
+#define WIN32_MEAN_AND_LEAN
+#include <windows.h>
+#endif
 
 using namespace falco::app;
 using namespace falco::app::actions;
