8de1130bec4e8052a83153fd23cf56ac9a43bf02
Jason Dellaluce <jasondellaluce@gmail.com>
2023-11-15 16:31:28 +0000

    update(userspace/engine): bump engine version
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- userspace/engine/falco_engine_version.h
+++ userspace/engine/falco_engine_version.h
@@ -20,7 +20,7 @@ limitations under the License.
 
 // The version of this Falco engine
 #define FALCO_ENGINE_VERSION_MAJOR 0
-#define FALCO_ENGINE_VERSION_MINOR 26
+#define FALCO_ENGINE_VERSION_MINOR 27
 #define FALCO_ENGINE_VERSION_PATCH 0
 
 #define FALCO_ENGINE_VERSION \
@@ -34,4 +34,4 @@ limitations under the License.
 // It represents the fields supported by this version of Falco,
 // the event types, and the underlying driverevent schema. It's used to
 // detetect changes in engine version in our CI jobs.
-#define FALCO_ENGINE_CHECKSUM "df5b0b40d3e1dafc0de13459a4b889f8680ec154690f445952e74799920ae380"
+#define FALCO_ENGINE_CHECKSUM "dbc34e88ab420320994d85f155dee6baff2dd018aacc00e249f897edc8b1e0f4"
