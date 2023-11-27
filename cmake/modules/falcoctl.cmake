5eb2ae8d76f25ab89a615c9a6258ecc8399c71f8
Federico Di Pierro <nierro92@gmail.com>
2023-11-27 16:30:17 +0100

    chore(cmake/modules): bumped falcoctl to latest alpha.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- cmake/modules/falcoctl.cmake
+++ cmake/modules/falcoctl.cmake
@@ -16,20 +16,19 @@ include(ExternalProject)
 
 string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME} FALCOCTL_SYSTEM_NAME)
 
-set(FALCOCTL_VERSION "0.6.2")
+set(FALCOCTL_VERSION "0.7.0-alpha1")
 
 if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     set(FALCOCTL_SYSTEM_PROC_GO "amd64")
-    set(FALCOCTL_HASH "2d06d7577dbae91fb085f71477ff6e22076a815978bddd036984fa077236a515")
+    set(FALCOCTL_HASH "5ed9034a4111dad3b28f15ebf0470b1b2038107ace4824de0540b0e0c54003be")
 else() # aarch64
     set(FALCOCTL_SYSTEM_PROC_GO "arm64")
-    set(FALCOCTL_HASH "0b711a1b3499f479d999f4f4d2c94fc4f0bc23a2506711b613e6eedb0593631b")
+    set(FALCOCTL_HASH "e3c01f75a33ac7b9151326b16a77b346334b59c14a0c87e1056cbcf6579f1478")
 endif()
 
 ExternalProject_Add(
         falcoctl
-        URL "https://github.com/falcosecurity/falcoctl/archive/555594a2860284947ff83eefd4bd9a5abc6e9fe1.zip"
-        #URL "https://github.com/falcosecurity/falcoctl/releases/download/v${FALCOCTL_VERSION}/falcoctl_${FALCOCTL_VERSION}_${FALCOCTL_SYSTEM_NAME}_${FALCOCTL_SYSTEM_PROC_GO}.tar.gz"
+        URL "https://github.com/falcosecurity/falcoctl/releases/download/v${FALCOCTL_VERSION}/falcoctl_${FALCOCTL_VERSION}_${FALCOCTL_SYSTEM_NAME}_${FALCOCTL_SYSTEM_PROC_GO}.tar.gz"
         #URL_HASH "SHA256=${FALCOCTL_HASH}"
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
