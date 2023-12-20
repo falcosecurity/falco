81e956864302caf82504c6f6c62c53a356aeb08b
Luca Guerra <luca@guerra.sh>
2023-12-20 11:32:24 +0000

    chore(engine): bump engine version
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- userspace/engine/falco_engine_version.h
+++ userspace/engine/falco_engine_version.h
@@ -20,7 +20,7 @@ limitations under the License.
 
 // The version of this Falco engine
 #define FALCO_ENGINE_VERSION_MAJOR 0
-#define FALCO_ENGINE_VERSION_MINOR 30
+#define FALCO_ENGINE_VERSION_MINOR 31
 #define FALCO_ENGINE_VERSION_PATCH 0
 
 #define FALCO_ENGINE_VERSION \
