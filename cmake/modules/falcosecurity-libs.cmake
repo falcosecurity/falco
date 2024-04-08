7d2887c19d9d82772102002672310ab37ac1bb0e
Luca Guerra <luca@guerra.sh>
2024-04-08 14:19:20 +0000

    chore(falco): update falco libs to latest master
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- cmake/modules/falcosecurity-libs.cmake
+++ cmake/modules/falcosecurity-libs.cmake
@@ -35,8 +35,8 @@ else()
   # In case you want to test against another falcosecurity/libs version (or branch, or commit) just pass the variable -
   # ie., `cmake -DFALCOSECURITY_LIBS_VERSION=dev ..`
   if(NOT FALCOSECURITY_LIBS_VERSION)
-    set(FALCOSECURITY_LIBS_VERSION "026ffe1d8f1b25c6ccdc09afa2c02afdd3e3f672")
-    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=127ea015e24e3f782ca443649fde1f6f0bc1966b8812c54de45415e992946e7e")
+    set(FALCOSECURITY_LIBS_VERSION "4893cdb9da333dfd850cc2d677916d796bfc47f6")
+    set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=130dbf120245c39da2dd85cd8a3ebdec49463d00f7b474230795089cc73a6450")
   endif()
 
   # cd /path/to/build && cmake /path/to/source
