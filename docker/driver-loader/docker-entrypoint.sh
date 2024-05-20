130706155433aa0a00c0fafffd22bbb75154991c
Federico Di Pierro <nierro92@gmail.com>
2024-05-20 14:21:38 +0200

    new(docker): initial work towards new docker images.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- docker/driver-loader/docker-entrypoint.sh
+++ docker/driver-loader/docker-entrypoint.sh
@@ -136,3 +136,5 @@ else
 fi
 
 /usr/bin/falcoctl driver install --compile=$ENABLE_COMPILE --download=$ENABLE_DOWNLOAD --http-insecure=$HTTP_INSECURE --http-headers="$FALCOCTL_DRIVER_HTTP_HEADERS" $extra_args
+
+exec /usr/bin/falco
