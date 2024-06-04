c47b5d13d06967e270b11aca603e449a449d9bd1
Gianmatteo Palmieri <mail@gian.im>
2024-06-04 18:02:00 +0200

    Revert "fix(engine): apply output substitutions for all sources"
    This reverts commit 4ef7c9553aaea2bfe75aa867650c7e9745c2909f.
    
    Signed-off-by: Gianmatteo Palmieri <mail@gian.im>

--- userspace/engine/rule_loader_compiler.cpp
+++ userspace/engine/rule_loader_compiler.cpp
@@ -509,7 +509,10 @@ void rule_loader::compiler::compile_rule_infos(
 
 		// build rule output message
 		rule.output = r.output;
-		apply_output_substitutions(cfg, rule.output);
+		if (r.source == falco_common::syscall_source)
+		{
+			apply_output_substitutions(cfg, rule.output);
+		}
 
 		// validate the rule's output
 		if(!is_format_valid(*cfg.sources.at(r.source), rule.output, err))
