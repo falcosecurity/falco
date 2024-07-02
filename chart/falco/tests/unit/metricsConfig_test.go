ceec09d9615ea088c52cb9ec37b3882b502d506f
Aldo Lacuku <aldo@lacuku.eu>
2024-07-02 14:55:01 +0200

    feat(falco): add support for Falco metrics
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

---
+++ chart/falco/tests/unit/metricsConfig_test.go
@@ -0,0 +1,204 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unit
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+)
+
+type metricsConfig struct {
+	Enabled                    bool   `yaml:"enabled"`
+	ConvertMemoryToMB          bool   `yaml:"convert_memory_to_mb"`
+	IncludeEmptyValues         bool   `yaml:"include_empty_values"`
+	KernelEventCountersEnabled bool   `yaml:"kernel_event_counters_enabled"`
+	ResourceUtilizationEnabled bool   `yaml:"resource_utilization_enabled"`
+	RulesCountersEnabled       bool   `yaml:"rules_counters_enabled"`
+	LibbpfStatsEnabled         bool   `yaml:"libbpf_stats_enabled"`
+	OutputRule                 bool   `yaml:"output_rule"`
+	StateCountersEnabled       bool   `yaml:"state_counters_enabled"`
+	Interval                   string `yaml:"interval"`
+}
+
+type webServerConfig struct {
+	Enabled                  bool   `yaml:"enabled"`
+	K8sHealthzEndpoint       string `yaml:"k8s_healthz_endpoint"`
+	ListenPort               string `yaml:"listen_port"`
+	PrometheusMetricsEnabled bool   `yaml:"prometheus_metrics_enabled"`
+	SSLCertificate           string `yaml:"ssl_certificate"`
+	SSLEnabled               bool   `yaml:"ssl_enabled"`
+	Threadiness              int    `yaml:"threadiness"`
+}
+
+func TestMetricsConfigInFalcoConfig(t *testing.T) {
+	t.Parallel()
+
+	helmChartPath, err := filepath.Abs(chartPath)
+	require.NoError(t, err)
+
+	testCases := []struct {
+		name     string
+		values   map[string]string
+		expected func(t *testing.T, metricsConfig, webServerConfig any)
+	}{
+		{
+			"defaultValues",
+			nil,
+			func(t *testing.T, metricsConfig, webServerConfig any) {
+				require.Len(t, metricsConfig, 10, "should have ten items")
+
+				metrics, err := getMetricsConfig(metricsConfig)
+				require.NoError(t, err)
+				require.NotNil(t, metrics)
+				require.True(t, metrics.ConvertMemoryToMB)
+				require.False(t, metrics.Enabled)
+				require.False(t, metrics.IncludeEmptyValues)
+				require.True(t, metrics.KernelEventCountersEnabled)
+				require.True(t, metrics.ResourceUtilizationEnabled)
+				require.True(t, metrics.RulesCountersEnabled)
+				require.Equal(t, "1h", metrics.Interval)
+				require.True(t, metrics.LibbpfStatsEnabled)
+				require.True(t, metrics.OutputRule)
+				require.True(t, metrics.StateCountersEnabled)
+
+				webServer, err := getWebServerConfig(webServerConfig)
+				require.NoError(t, err)
+				require.NotNil(t, webServer)
+				require.True(t, webServer.Enabled)
+				require.False(t, webServer.PrometheusMetricsEnabled)
+			},
+		},
+		{
+			"metricsEnabled",
+			map[string]string{
+				"metrics.enabled": "true",
+			},
+			func(t *testing.T, metricsConfig, webServerConfig any) {
+				require.Len(t, metricsConfig, 10, "should have ten items")
+
+				metrics, err := getMetricsConfig(metricsConfig)
+				require.NoError(t, err)
+				require.NotNil(t, metrics)
+				require.True(t, metrics.ConvertMemoryToMB)
+				require.True(t, metrics.Enabled)
+				require.False(t, metrics.IncludeEmptyValues)
+				require.True(t, metrics.KernelEventCountersEnabled)
+				require.True(t, metrics.ResourceUtilizationEnabled)
+				require.True(t, metrics.RulesCountersEnabled)
+				require.Equal(t, "1h", metrics.Interval)
+				require.True(t, metrics.LibbpfStatsEnabled)
+				require.False(t, metrics.OutputRule)
+				require.True(t, metrics.StateCountersEnabled)
+
+				webServer, err := getWebServerConfig(webServerConfig)
+				require.NoError(t, err)
+				require.NotNil(t, webServer)
+				require.True(t, webServer.Enabled)
+				require.True(t, webServer.PrometheusMetricsEnabled)
+			},
+		},
+		{
+			"Flip/Change Values",
+			map[string]string{
+				"metrics.enabled":                    "true",
+				"metrics.convertMemoryToMB":          "false",
+				"metrics.includeEmptyValues":         "true",
+				"metrics.kernelEventCountersEnabled": "false",
+				"metrics.resourceUtilizationEnabled": "false",
+				"metrics.rulesCountersEnabled":       "false",
+				"metrics.libbpfStatsEnabled":         "false",
+				"metrics.outputRule":                 "false",
+				"metrics.stateCountersEnabled":       "false",
+				"metrics.interval":                   "1s",
+			},
+			func(t *testing.T, metricsConfig, webServerConfig any) {
+				require.Len(t, metricsConfig, 10, "should have ten items")
+
+				metrics, err := getMetricsConfig(metricsConfig)
+				require.NoError(t, err)
+				require.NotNil(t, metrics)
+				require.False(t, metrics.ConvertMemoryToMB)
+				require.True(t, metrics.Enabled)
+				require.True(t, metrics.IncludeEmptyValues)
+				require.False(t, metrics.KernelEventCountersEnabled)
+				require.False(t, metrics.ResourceUtilizationEnabled)
+				require.False(t, metrics.RulesCountersEnabled)
+				require.Equal(t, "1s", metrics.Interval)
+				require.False(t, metrics.LibbpfStatsEnabled)
+				require.False(t, metrics.OutputRule)
+				require.False(t, metrics.StateCountersEnabled)
+
+				webServer, err := getWebServerConfig(webServerConfig)
+				require.NoError(t, err)
+				require.NotNil(t, webServer)
+				require.True(t, webServer.Enabled)
+				require.True(t, webServer.PrometheusMetricsEnabled)
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			options := &helm.Options{SetValues: testCase.values}
+			output := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/configmap.yaml"})
+
+			var cm corev1.ConfigMap
+			helm.UnmarshalK8SYaml(t, output, &cm)
+			var config map[string]interface{}
+
+			helm.UnmarshalK8SYaml(t, cm.Data["falco.yaml"], &config)
+			metrics := config["metrics"]
+			webServer := config["webserver"]
+			testCase.expected(t, metrics, webServer)
+		})
+	}
+}
+
+func getMetricsConfig(config any) (*metricsConfig, error) {
+	var metrics metricsConfig
+
+	metricsByte, err := yaml.Marshal(config)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(metricsByte, &metrics); err != nil {
+		return nil, err
+	}
+
+	return &metrics, nil
+}
+
+func getWebServerConfig(config any) (*webServerConfig, error) {
+	var webServer webServerConfig
+	webServerByte, err := yaml.Marshal(config)
+	if err != nil {
+		return nil, err
+	}
+	if err := yaml.Unmarshal(webServerByte, &webServer); err != nil {
+		return nil, err
+	}
+	return &webServer, nil
+}
---
+++ chart/falco/tests/unit/serviceMonitorTemplate_test.go
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unit
+
+import (
+	"encoding/json"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type serviceMonitorTemplateTest struct {
+	suite.Suite
+	chartPath   string
+	releaseName string
+	namespace   string
+	templates   []string
+}
+
+func TestServiceMonitorTemplate(t *testing.T) {
+	t.Parallel()
+
+	chartFullPath, err := filepath.Abs(chartPath)
+	require.NoError(t, err)
+
+	suite.Run(t, &serviceMonitorTemplateTest{
+		Suite:       suite.Suite{},
+		chartPath:   chartFullPath,
+		releaseName: "falco-test",
+		namespace:   "falco-namespace-test",
+		templates:   []string{"templates/serviceMonitor.yaml"},
+	})
+}
+
+func (s *serviceMonitorTemplateTest) TestCreationDefaultValues() {
+	// Render the servicemonitor and check that it has not been rendered.
+	_, err := helm.RenderTemplateE(s.T(), &helm.Options{}, s.chartPath, s.releaseName, s.templates)
+	s.Error(err, "should error")
+	s.Equal("error while running command: exit status 1; Error: could not find template templates/serviceMonitor.yaml in chart", err.Error())
+}
+
+func (s *serviceMonitorTemplateTest) TestEndpoint() {
+	defaultEndpointsJSON := `[
+    {
+        "port": "metrics",
+        "interval": "15s",
+        "scrapeTimeout": "10s",
+        "honorLabels": true,
+        "path": "/metrics",
+        "scheme": "http"
+    }
+]`
+	var defaultEndpoints []monitoringv1.Endpoint
+	err := json.Unmarshal([]byte(defaultEndpointsJSON), &defaultEndpoints)
+	s.NoError(err)
+
+	options := &helm.Options{SetValues: map[string]string{"serviceMonitor.create": "true"}}
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.releaseName, s.templates)
+
+	var svcMonitor monitoringv1.ServiceMonitor
+	helm.UnmarshalK8SYaml(s.T(), output, &svcMonitor)
+
+	s.Len(svcMonitor.Spec.Endpoints, 1, "should have only one endpoint")
+	s.True(reflect.DeepEqual(svcMonitor.Spec.Endpoints[0], defaultEndpoints[0]))
+}
+
+func (s *serviceMonitorTemplateTest) TestNamespaceSelector() {
+	options := &helm.Options{SetValues: map[string]string{"serviceMonitor.create": "true"}}
+	output := helm.RenderTemplate(s.T(), options, s.chartPath, s.releaseName, s.templates)
+
+	var svcMonitor monitoringv1.ServiceMonitor
+	helm.UnmarshalK8SYaml(s.T(), output, &svcMonitor)
+	s.Len(svcMonitor.Spec.NamespaceSelector.MatchNames, 1)
+	s.Equal("default", svcMonitor.Spec.NamespaceSelector.MatchNames[0])
+}
