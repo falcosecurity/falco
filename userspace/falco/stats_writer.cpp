90a5fd6bcb5248c8932ef16c92e16ab42adcb471
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2024-09-23 12:33:20 +0200

    fix(falco_metrics): remove ifinfo_json stat/metric
    Using JSON as value prevents any meaningful aggregation for the stats.
    Splitting these information into multiple labels can drastically
    increase the number of dimensions, as the number of interfaces and
    addresses can be high in some environment. Moreover, these information
    are not currently refreshed, even if they can frequently change. Given
    these reasons, remove ifinfo_json from stats and metrics.
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- userspace/falco/stats_writer.cpp
+++ userspace/falco/stats_writer.cpp
@@ -358,39 +358,6 @@ void stats_writer::collector::get_metrics_output_fields_wrapper(
 		output_fields[metric_name_file_sha256] = item.second;
 	}
 
-	auto ipv4list = inspector->get_ifaddr_list().get_ipv4_list();
-	auto ipv6list = inspector->get_ifaddr_list().get_ipv6_list();
-	nlohmann::json ipv4_json;
-	nlohmann::json ipv6_json;
-	if(ipv4list)
-	{
-		for (const auto& item : *ipv4list)
-		{
-			if(item.m_name == "lo")
-			{
-				continue;
-			}
-			ipv4_json[item.m_name] = item.addr_to_string();
-		}
-	}
-
-	if(ipv6list)
-	{
-		for (const auto& item : *ipv6list)
-		{
-			if(item.m_name == "lo")
-			{
-				continue;
-			}
-			ipv6_json[item.m_name] = item.addr_to_string();
-		}
-	}
-	nlohmann::json ifinfo_json;
-	ifinfo_json["ipv4"] = ipv4_json;
-	ifinfo_json["ipv6"] = ipv6_json;
-	m_ifinfo_json_escaped = ifinfo_json.dump();
-	output_fields["falco.host_ifinfo_json"] = m_ifinfo_json_escaped;
-
 #endif
 	output_fields["evt.source"] = src;
 	for (size_t i = 0; i < sizeof(all_driver_engines) / sizeof(const char*); i++)
