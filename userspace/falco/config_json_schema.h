5ebfa1b05b01b8bd60724e6975d1115f1eed62f4
Leonardo Grasso <me@leonardograsso.com>
2025-07-18 13:00:55 +0200

    new: add config options and docs for `capture` feature
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- userspace/falco/config_json_schema.h
+++ userspace/falco/config_json_schema.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 /*
-Copyright (C) 2024 The Falco Authors.
+Copyright (C) 2025 The Falco Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -310,6 +310,29 @@ const char config_schema_string[] = LONG_STRING_CONST(
                     "type": "boolean"
                 }
             }
+        },
+        "Capture": {
+            "type": "object",
+            "additionalProperties": false,
+            "properties": {
+                "enabled": {
+                    "type": "boolean"
+                },
+                "path_prefix": {
+                    "type": "string"
+                },
+                "mode": {
+                    "type": "string",
+                    "enum": [
+                      "rules",
+                      "all_rules"
+                    ]
+                },
+                "default_duration": {
+                    "type": "integer"
+                }
+            },
+            "title": "Capture"
         },
         "BaseSyscalls": {
             "type": "object",
