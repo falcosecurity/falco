6791b5dbc4a2b57892fd9bbfec456c7a4c5942db
Leonardo Grasso <me@leonardograsso.com>
2025-07-24 14:54:46 +0200

    update(charts/falco): bump container plugin to v0.3.3
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- chart/falco/tests/unit/k8smetaPlugin/k8smetacollectorDependency_test.go
+++ chart/falco/tests/unit/k8smetaPlugin/k8smetacollectorDependency_test.go
@@ -591,7 +591,7 @@ func TestFalcoctlRefs(t *testing.T) {
 		require.Len(t, refs, 3)
 		require.True(t, slices.Contains(refs, "falco-rules:4"))
 		require.True(t, slices.Contains(refs, "ghcr.io/falcosecurity/plugins/plugin/k8smeta:0.3.0"))
-		require.True(t, slices.Contains(refs, "ghcr.io/falcosecurity/plugins/plugin/container:0.3.2"))
+		require.True(t, slices.Contains(refs, "ghcr.io/falcosecurity/plugins/plugin/container:0.3.3"))
 	}
 
 	testCases := []struct {
