a462689561c0a98e712cc1bfd0c6e7d6d33c25a8
Leonardo Grasso <me@leonardograsso.com>
2025-07-28 10:21:59 +0200

    update(userspace/engine): bump engine version to 0.55.0
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- userspace/engine/falco_engine_version.h
+++ userspace/engine/falco_engine_version.h
@@ -20,7 +20,7 @@ limitations under the License.
 
 // The version of this Falco engine
 #define FALCO_ENGINE_VERSION_MAJOR 0
-#define FALCO_ENGINE_VERSION_MINOR 54
+#define FALCO_ENGINE_VERSION_MINOR 55
 #define FALCO_ENGINE_VERSION_PATCH 0
 
 #define FALCO_ENGINE_VERSION                                                               \
