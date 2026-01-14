da8644b73e6f6866073595c09f7103d4c60aea03
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2026-01-14 12:53:41 +0100

    chore(cmake): bump libs/drivers to `0.23.1`/`9.1.0+driver`
    The previously used driver version was already the `9.1.0+driver`:
    simply replace the commit SHA with the release name.
    
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>

--- cmake/modules/driver.cmake
+++ cmake/modules/driver.cmake
@@ -35,9 +35,9 @@ else()
 	# FALCOSECURITY_LIBS_VERSION. In case you want to test against another driver version (or
 	# branch, or commit) just pass the variable - ie., `cmake -DDRIVER_VERSION=dev ..`
 	if(NOT DRIVER_VERSION)
-		set(DRIVER_VERSION "9e6a8ccd4e4f5796f45ad486decd00b4996129b7")
+		set(DRIVER_VERSION "9.1.0+driver")
 		set(DRIVER_CHECKSUM
-			"SHA256=87902814e29718529094b89ff2a3ddbd4ee7aa77da824d4acbaad0d863e04ce9"
+			"SHA256=14cba5b610bf48cd0a0a94b1156ed86bfb552c7ed24b68b1028360fa3af18cbb"
 		)
 	endif()
 
