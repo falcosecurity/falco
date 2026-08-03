e7a34cced6e7b1335c8ae38ca6c6b75d1ed0bc31
poiana <51138685+poiana@users.noreply.github.com>
2026-08-03 07:39:34 +0000

    update(cmake): update libs and driver to latest master.
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- cmake/modules/falcosecurity-libs.cmake
+++ cmake/modules/falcosecurity-libs.cmake
@@ -42,9 +42,9 @@ else()
 	# version (or branch, or commit) just pass the variable - ie., `cmake
 	# -DFALCOSECURITY_LIBS_VERSION=dev ..`
 	if(NOT FALCOSECURITY_LIBS_VERSION)
-		set(FALCOSECURITY_LIBS_VERSION "c6e56483b4228bd5c9943cb26aea5f4312df0f03")
+		set(FALCOSECURITY_LIBS_VERSION "1a1d1e7ec9374cff24e9d03f55422590acb8fc92")
 		set(FALCOSECURITY_LIBS_CHECKSUM
-			"SHA256=adc5a7b125a49f7b6a6fce287464b235a69e1cfe24c29ecff49d3999eeb7599c"
+			"SHA256=ead92e48b21e30db79324dba7c267f22083ecc7510702cc9920c912c516afe05"
 		)
 	endif()
 
